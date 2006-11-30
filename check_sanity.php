ed326f398d6b2084ac76907d2c9582fbea0eeaae
Matthew Smith <m.smith@library.uq.edu.au>
2006-11-30 06:30:12 +0000

    Password protected the sanity check as some failure messages may expose passwords or server settings.

--- check_sanity.php
+++ check_sanity.php
@@ -14,9 +14,26 @@ include_once(APP_INC_PATH.'class.template.php');
 
 $tpl = new Template_API();
 $tpl->setTemplate('sanity_check.tpl.html');
+
+
+Auth::checkAuthentication(APP_SESSION);
+
+
+$username = Auth::getUsername();
+$tpl->assign("isUser", $username);
+$isAdministrator = User::isUserAdministrator($username);
+$tpl->assign("isAdministrator", $isAdministrator);
+if (Auth::userExists($username)) { // if the user is registered as a Fez user
+    $tpl->assign("isFezUser", $username);
+}
+if (!$isAdministrator) {
+	$tpl->assign('show_not_allowed_message', true);
+} else {
+    $res = SanityChecks::runAllChecks();
+    $tpl->assign('sanity_results',$res);
+	
+}
  
-$res = SanityChecks::runAllChecks();
-$tpl->assign('sanity_results',$res);
 
 $tpl->displayTemplate();
 
