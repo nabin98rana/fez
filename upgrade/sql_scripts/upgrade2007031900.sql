34ad5e3d1341486c2d89dce6b3b385612cca7415
Matthew Smith <m.smith@library.uq.edu.au>
2007-03-20 05:43:16 +0000

    fixed typo in citations upgrade script

--- upgrade/sql_scripts/upgrade2007031900.sql
+++ upgrade/sql_scripts/upgrade2007031900.sql
@@ -1,4 +1,4 @@
-create table `%TABLE_PREFIX%citations` 
+create table `%TABLE_PREFIX%citation` 
         (  `cit_id` int NOT NULL AUTO_INCREMENT , 
            `cit_xdis_id` int NOT NULL , 
            `cit_template` text NOT NULL , 
