5450e15e5229707a4a4076381db61e3018c884d8
Christiaan Kortekaas <ck@uq.edu.au>
2007-05-01 01:25:33 +0000

    - Fixed IF nesting for new file attachment DC:format oai datastream thing.

--- include/class.oai.php
+++ include/class.oai.php
@@ -299,14 +299,14 @@ class OAI
 					if ($result['sek_title'] == 'File Attachment Name') { 
 						if (!is_array(@$return[$result['rmf_rec_pid']]['File_Attachment'])) {
 							$return[$result['rmf_rec_pid']]['File_Attachment'] = array();
-						}	
+						}						
+					    if ((!is_numeric(strpos($result['rmf_'.$result['xsdmf_data_type']], "thumbnail_"))) && (!is_numeric(strpos($result['rmf_'.$result['xsdmf_data_type']], "web_"))) && (!is_numeric(strpos($result['rmf_'.$result['xsdmf_data_type']], "preview_"))) && (!is_numeric(strpos($result['rmf_'.$result['xsdmf_data_type']], "presmd_"))) && (!is_numeric(strpos($result['rmf_'.$result['xsdmf_data_type']], "FezACML_"))) )   {
+					    	if (!in_array($result['rmf_'.$result['xsdmf_data_type']], $return[$result['rmf_rec_pid']]['File_Attachment'])) {
+								array_push($return[$result['rmf_rec_pid']]['File_Attachment'], $result['rmf_'.$result['xsdmf_data_type']]);
+								$return[$result['rmf_rec_pid']]['oai_dc'] .=  '<dc:format>'.APP_BASE_URL.'eserv.php?pid='.$result['rmf_rec_pid'].'&amp;dsID='.$result['rmf_'.$result['xsdmf_data_type']].'</dc:format>'."\n";
+					    	}
+					    }
 					}
-				    if ((!is_numeric(strpos($result['rmf_'.$result['xsdmf_data_type']], "thumbnail_"))) && (!is_numeric(strpos($result['rmf_'.$result['xsdmf_data_type']], "web_"))) && (!is_numeric(strpos($result['rmf_'.$result['xsdmf_data_type']], "preview_"))) && (!is_numeric(strpos($result['rmf_'.$result['xsdmf_data_type']], "presmd_"))) && (!is_numeric(strpos($result['rmf_'.$result['xsdmf_data_type']], "FezACML_"))) )   {
-				    	if (!in_array($result['rmf_'.$result['xsdmf_data_type']], $return[$result['rmf_rec_pid']]['File_Attachment'])) {
-							array_push($return[$result['rmf_rec_pid']]['File_Attachment'], $result['rmf_'.$result['xsdmf_data_type']]);
-							$return[$result['rmf_rec_pid']]['oai_dc'] .=  '<dc:format>'.APP_BASE_URL.'eserv.php?pid='.$result['rmf_rec_pid'].'&amp;dsID='.$result['rmf_'.$result['xsdmf_data_type']].'</dc:format>'."\n";
-				    	}
-				    }
 					if (!is_array(@$return[$result['rmf_rec_pid']]['datastreams'])) {
 						$return[$result['rmf_rec_pid']]['datastreams'] = array();
 					}
