0ecd1c9002da104ab3a2faddfa46ed39d2fad70c
Lachlan Kuhn <lachlan@uq.edu.au>
2007-06-06 05:24:05 +0000

    Added SQL upgrade script to add sek_lookup_function column to search_keys table, and populate selected rows with default data.

---
+++ upgrade/sql_scripts/upgrade2007060602.sql
@@ -0,0 +1,34 @@
+ALTER TABLE `%TABLE_PREFIX%search_key` 
+  ADD COLUMN `sek_lookup_function` varchar(255) default NULL;
+
+UPDATE `%TABLE_PREFIX%search_key` 
+  SET sek_lookup_function = 'Controlled_Vocab::getTitle' 
+  WHERE sek_title = 'Subject';
+
+UPDATE `%TABLE_PREFIX%search_key` 
+  SET sek_lookup_function = 'Status::getTitle' 
+  WHERE sek_title = 'Status';
+
+UPDATE `%TABLE_PREFIX%search_key` 
+  SET sek_lookup_function = 'Object_Type::getTitle' 
+  WHERE sek_title = 'Object Type';
+
+UPDATE `%TABLE_PREFIX%search_key` 
+  SET sek_lookup_function = 'XSD_Display::getTitle' 
+  WHERE sek_title = 'Display Type';
+
+UPDATE `%TABLE_PREFIX%search_key` 
+  SET sek_lookup_function = 'User::getFullName' 
+  WHERE sek_title = 'Depositor';
+
+UPDATE `%TABLE_PREFIX%search_key` 
+  SET sek_lookup_function = 'User::getFullName' 
+  WHERE sek_title = 'Assigned User ID';
+
+UPDATE `%TABLE_PREFIX%search_key` 
+  SET sek_lookup_function = 'Group::getTitle' 
+  WHERE sek_title = 'Assigned Group ID';
+
+UPDATE `%TABLE_PREFIX%search_key` 
+  SET sek_lookup_function = 'Author::getFullName' 
+  WHERE sek_title = 'Author ID';
