b58a0128207f5963eaf91331bdedfc67aa8f031c
Matthew Smith <m.smith@library.uq.edu.au>
2007-08-22 02:57:11 +0000

    Fixed the reindex collection workflow.

--- include/class.bgp_index_object.php
+++ include/class.bgp_index_object.php
@@ -16,6 +16,7 @@ include_once(APP_INC_PATH . "class.reindex.php");
 
 @define('INDEX_TYPE_FEDORAINDEX', 1);
 @define('INDEX_TYPE_REINDEX', 2);
+@define('INDEX_TYPE_REINDEX_OBJECTS', 3);  // index specific pids
 
 
 class BackgroundProcess_Index_Object extends BackgroundProcess
@@ -51,10 +52,51 @@ class BackgroundProcess_Index_Object extends BackgroundProcess
         $reindex->bgp = $this;
         if ($index_type == INDEX_TYPE_FEDORAINDEX) {
             $reindex->reindexMissingList($params,$terms);
-        } else {
+        } elseif ($index_type == INDEX_TYPE_REINDEX_OBJECTS) {
+        	if (!empty($pid)) {
+        		$source_pids = array();
+        		$parent_pids = array($pid);
+        		for ($ii = 0; $ii < count($parent_pids); $ii++) {
+        			$record = new RecordGeneral($pid);
+        			if ($record->isCollection()) {
+        				$source_pids = array_unique(array_merge($source_pids, $record->getChildrenPids()));
+    				} elseif ($record->isCommunity()) {
+    					$parent_pids = array_merge($parent_pids, $record->getChildrenPids());
+					} else {
+        				$source_pids = array_unique(array_merge($source_pids, array($pid)));
+					}
+				}
+				$this->setProgress(1);
+				$this->setStatus("Reindexing ".count($source_pids)." items.");
+				$reindex_record_counter = 0;
+				$record_count = count($source_pids);
+				foreach ($source_pids as $source_pid) {
+		            $reindex_record_counter++;
+
+                    $bgp_details = $this->getDetails();
+                    $utc_date = Date_API::getSimpleDateUTC();
+                    $time_per_object = Date_API::dateDiff("s", $bgp_details['bgp_started'], $utc_date);
+                    $date_new = new Date(strtotime($bgp_details['bgp_started']));
+                    $time_per_object = round(($time_per_object / $reindex_record_counter), 2);
+                    //$expected_finish = Date_API::getFormattedDate($date_new->getTime());
+                    $date_new->addSeconds($time_per_object*$record_count);
+                    $tz = Date_API::getPreferredTimezone($bgp_details["bgp_usr_id"]);
+    				$res[$key]["bgp_started"] = Date_API::getFormattedDate($res[$key]["bgp_started"], $tz);
+                    $expected_finish = Date_API::getFormattedDate($date_new->getTime(), $tz);
+					$this->setProgress(intval(100*$reindex_record_counter/$record_count));
+                    $this->setStatus("Reindexing:  '".$source_pid."'  (".$reindex_record_counter."/".$record_count.") (Avg ".$time_per_object."s per Object, Expected Finish ".$expected_finish.")");
+
+					$params['items'] = array($source_pid);
+					$params['rebuild'] = false;
+					$reindex->indexFezFedoraObjects($params);
+				}
+				$this->setProgress(100);
+				$this->setStatus("Reindexed ".count($source_pids)." items.");
+        	}
+        } else  {
             $reindex->reindexFullList($params,$terms);
         }
-        $this->setState(2);        
+        $this->setState(2);        // done
     }
 }
  
--- workflow/regenerate_object_index.php
+++ workflow/regenerate_object_index.php
@@ -11,12 +11,11 @@
 include_once(APP_INC_PATH. 'class.auth.php');
 include_once(APP_INC_PATH . "class.bgp_index_object.php");
 
-$terms = "$this->pid";
-$params = array();
-
-$bgp = new BackgroundProcess_Index_Object;
-$inputs = compact('params','terms');
+$pid = $this->pid;
+$index_type = INDEX_TYPE_REINDEX_OBJECTS;
+$inputs = compact('pid','index_type');
 $inputs_str = serialize($inputs);
+$bgp = new BackgroundProcess_Index_Object;
 $bgp->register($inputs_str, Auth::getUserID());
 
 ?>
