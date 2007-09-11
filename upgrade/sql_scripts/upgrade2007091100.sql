51eb3bc6a890430f0d0c0a817f7e69cd189c8e53
Matthew Smith <m.smith@library.uq.edu.au>
2007-09-11 02:22:15 +0000

    Added upgrade script to get all search key tables into the utf8_general_ci collation

---
+++ upgrade/sql_scripts/upgrade2007091100.sql
@@ -0,0 +1,58 @@
+alter table %TABLE_PREFIX%record_search_key
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_alternative_title
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_assigned_group_id
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_assigned_user_id
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_author
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_author_id
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_book_title
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_conference_name
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_contributor
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_contributor_id
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_file_attachment_content
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_file_attachment_name
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_file_downloads
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_identifier
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_isannotationof
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_isdatacomponentof
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_isderivationof
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_ismemberof
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_journal_name
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_keywords
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_newspaper
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_notes
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_publisher
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_refereed
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_research_program
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_series
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_subject
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_xsd_display_option
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
+alter table %TABLE_PREFIX%record_search_key_assigned_user_id
+CONVERT TO CHARACTER SET utf8 COLLATE utf8_general_ci;
