427accec033f41cc96c46342a0b87d7b3770da61
Lachlan Kuhn <lachlan@uq.edu.au>
2007-11-13 02:25:23 +0000

    Added 'Description' and 'Image Filename' fields to Organisational Structure table (again). Not sure how these went missing ...

---
+++ upgrade/sql_scripts/upgrade2007111300.sql
@@ -0,0 +1,2 @@
+ALTER TABLE %TABLE_PREFIX%org_structure
+   ADD COLUMN `org_desc` text DEFAULT NULL;
---
+++ upgrade/sql_scripts/upgrade2007111301.sql
@@ -0,0 +1,2 @@
+ALTER TABLE %TABLE_PREFIX%org_structure
+   ADD COLUMN `org_image_filename` varchar(512) default NULL;
