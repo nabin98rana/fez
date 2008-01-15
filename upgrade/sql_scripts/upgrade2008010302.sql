a1abbe47d19a1592fb76492f64b2f7021676d4ac
Lachlan Kuhn <lachlan@uq.edu.au>
2008-01-15 04:26:53 +0000

    - Modified an older SQL upgrade script. - Relates to issue #313424.

--- upgrade/sql_scripts/upgrade2008010302.sql
+++ upgrade/sql_scripts/upgrade2008010302.sql
@@ -2,7 +2,7 @@ create table %TABLE_PREFIX%user_comments (
      usc_id           integer       auto_increment primary key,
      usc_userid       integer       not null default 0,
      usc_pid          varchar(64)   not null default '',
-     usc_comment      text          not null default '',
+     usc_comment      text          not null,
      usc_rating       integer       not null default 0,
      usc_date_created timestamp     not null default CURRENT_TIMESTAMP
 );
