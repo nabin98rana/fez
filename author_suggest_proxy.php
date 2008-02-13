cf3bce1ebfcf28e18ec46b30d5f52f3bc101bf37
Rhys Palmer <r.palmer@library.uq.edu.au>
2008-02-13 06:20:46 +0000

    Removed debugging code.

--- author_suggest_proxy.php
+++ author_suggest_proxy.php
@@ -37,8 +37,6 @@ include_once(APP_INC_PATH . "class.misc.php");
 include_once(APP_INC_PATH . "class.setup.php");
 include_once(APP_INC_PATH . "class.author.php");
 
-Error_Handler::logError('ysearch_proxy', __FILE__, __LINE__);
-
 $suggestions = Author::suggest($_GET['query'], true);
 
 $suggestions = array(
