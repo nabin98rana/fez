05ab59c087af990883a90667e5b2df51441e6186
Rhys Palmer <r.palmer@library.uq.edu.au>
2008-03-28 02:55:24 +0000

    More eSpace features related to 'My Fez' area moved to trunk. Relates to issue #324753

---
+++ templates/en/js/list.tpl.js
@@ -0,0 +1,208 @@
+<script language="JavaScript" type="text/javascript">
+<!--
+
+{if $list_type == 'collection_records_list'}
+var page_url = '{$rel_url}collection/{$collection_pid}';
+{elseif $list_type == 'community_list'}
+var page_url = '{$rel_url}list';
+{elseif $list_type == 'collection_list'}
+var page_url = '{$rel_url}community/{$community_pid}';
+{else}
+var page_url = '{$rel_url}list';
+{/if}
+{literal}
+var current_page = {/literal}{if $list_info.current_page != ""}{$list_info.current_page}{else}0{/if}{literal};
+var last_page = {/literal}{if $list_info.last_page != ""}{$list_info.last_page}{else}0{/if}{literal};
+function assignItems(f)
+{
+    if (!hasOneChecked(f, 'item[]')) {
+        alert('Please choose which entries to assign.');
+        return false;
+    }
+    if (f.users.options[f.users.selectedIndex].value == '') {
+        alert('Please choose the user to assign these entries to.');
+        f.users.focus();
+        selectField(f, 'users');
+        return false;
+    }
+    var features = 'width=420,height=200,top=30,left=30,resizable=no,scrollbars=yes,toolbar=no,location=no,menubar=no,status=no';
+    var popupWin = window.open('', '_popup', features);
+    popupWin.focus();
+    f.action = '{/literal}{$rel_url}{literal}popup.php';
+    f.target = '_popup';
+    f.submit();
+}
+
+
+function createRecord(f, xdis_id_sel) {
+    f.action = '{/literal}{$rel_url}{literal}workflow/new.php';
+    xdis_id = getSelectedOption(f, xdis_id_sel.getAttribute('name'));
+    f.xdis_id.setAttribute('value', xdis_id);
+	f.submit();
+}
+
+function assignWorkflow(f) {
+    var features = 'width=420,height=200,top=30,left=30,resizable=no,scrollbars=yes,toolbar=no,location=no,menubar=no,status=no';
+    var popupWin = window.open('', '_popup', features);
+    popupWin.focus();
+    f.target = '_popup';
+    f.action = '{/literal}{$rel_url}{literal}popup.php';
+    f.submit();
+}
+ 
+function hideClosed(f)
+{
+    if (f.hide_closed.checked) {
+        window.location.href = page_url + "/" + replaceParam(window.location.href, 'hide_closed', '1');
+    } else {
+        window.location.href = c + "/" + replaceParam(window.location.href, 'hide_closed', '0');
+    }
+}
+
+function checkPageField(ev)
+{
+    // check if the user is trying to submit the form by hitting <enter>
+    if (((window.event) && (window.event.keyCode == 13)) ||
+            ((ev) && (ev.which == 13))) {
+        return false;
+    }
+}
+function goPage(f, new_page)
+{
+    if ((new_page > last_page+1) || (new_page <= 0) ||
+            (new_page == current_page+1) || (!isNumberOnly(new_page))) {
+        f.page.value = current_page+1;
+        return false;
+    }
+    setPage(new_page-1);
+}
+function setPage(new_page)
+{
+    if ((new_page > last_page) || (new_page < 0) ||
+            (new_page == current_page)) {
+        return false;
+    }
+    window.location.href = page_url + "/" + replaceParam(window.location.href, 'pager_row', new_page);
+}
+function downloadCSV()
+{
+    var f = this.document.csv_form;
+    f.submit();
+    return false;
+}
+
+function disableFields()
+{
+    var f = document.list_form;
+    if (current_page == 0) {
+        f.first.disabled = true;
+        f.previous.disabled = true;
+    }
+    if ((current_page == last_page) || (last_page <= 0)) {
+        f.next.disabled = true;
+        f.last.disabled = true;
+    }
+    if ((current_page == 0) && (last_page <= 0)) {
+        f.page.disabled = true;
+        f.go.disabled = true;
+    }
+}
+
+function orderAndSort(f, field1, field2) {
+	sort_by = getSelectedOption(f, field1);
+	sort_order = getSelectedOption(f, field2);
+	{/literal}
+	{if $browse_mode == 'collection' || $browse_mode == 'community'}
+		sortURL = '{$query_plus}{$query_reduced_sans_sort}sort_by=' + sort_by + '&sort_order=' + sort_order;
+	{else}
+		{if $$query_reduced_sans_sort != ''}
+			sortURL = '{$query_plus}{$query_reduced_sans_sort}&sort_by=' + sort_by + '&sort_order=' + sort_order;
+		{else}
+			sortURL = '{$query_plus}{$query_reduced_sans_sort}sort_by=' + sort_by + '&sort_order=' + sort_order;
+		{/if}
+
+	{/if}{literal}
+	window.location.href = sortURL;
+}
+
+function resizePagerNew(f)
+{
+	{/literal}
+    var pagesize = f.page_size.options[f.page_size.selectedIndex].value;
+	{if $query_reduced == ''}var queryReducedString = '';{else}var queryReducedString = '{$query_reduced}';{/if}
+	if (queryReducedString.length > 0) {literal}{{/literal}
+		var pagingURL = "{$query_plus}{$query_reduced}&rows=" + pagesize + "&pager_row=0";
+	{literal}}{/literal} else {literal}{{/literal}
+		var pagingURL = "{$query_plus}{$query_reduced}rows=" + pagesize + "&pager_row=0";
+	{literal}}{/literal}
+
+	var theTemplate = '{$template_mode}';
+//	if (theTemplate !== "0") {literal}{{/literal}
+//		pagingURL = pagingURL + '&tpl=' + theTemplate;
+//	{literal}}{/literal}
+    window.location.href = pagingURL;
+	{literal}
+}
+
+function setTemplateNew(f, field) {
+	{/literal}
+    var tpl = getSelectedOption(f, field);
+	{if $query_reduced == ''}var queryReducedString = '';{else}var queryReducedString = '{$query_reduced}';{/if}
+
+	if (queryReducedString.length > 0) {literal}{{/literal}
+		var templateURL = "{$query_plus}{$query_reduced}&rows={$list_info.page_rows}&pager_row={$list_info.current_page}&tpl=" + tpl;
+	{literal}}{/literal} else {literal}{{/literal}
+		var templateURL = "{$query_plus}{$query_reduced}rows={$list_info.page_rows}&pager_row={$list_info.current_page}&tpl=" + tpl;
+	{literal}}{/literal}
+    window.location.href = templateURL;
+	{literal}
+}
+
+function purgeObject(pid)
+{
+    if (!confirm('This action will permanently delete the selected object.')) {
+        return false;
+    } else {
+        window.location.replace('{/literal}{$rel_url}workflow/delete.php?pid=' + pid + '&href={$smarty.server.REQUEST_URI|escape:'url'}{literal}');
+        return true;
+    }
+}
+function checkDeleteRecords(f)
+{
+    if (!hasOneChecked(f, 'pids[]')) {
+        alert('Please select at least one item.');
+        return false;
+    }
+    if (!confirm('The checked items will be deleted, are you sure?')) {
+        return false;
+    } else {
+	f.cat.value = 'delete_objects';
+    f.action = '{/literal}{$rel_url}{literal}popup.php';
+    f.target = '_popup';
+	f.method = 'post';
+        var features = 'width=420,height=200,top=30,left=30,resizable=no,scrollbars=yes,toolbar=no,location=no,menubar=no,status=no';
+        var popupWin = window.open('', '_popup', features);
+        popupWin.focus();
+
+    f.submit();        
+        
+    }
+}
+var custom_filter_element = getPageElement('custom_filter_form' + '1');
+{/literal}
+/* {$browse_type} ---- {$list_type} */
+{if (($browse_type == "" && $list_type <> "all_records_list") || $browse_type != "")}
+{literal}
+if (isElementVisible(custom_filter_element)) {
+	toggleVisibility('custom_filter_form');
+}
+{/literal}
+{/if}
+{literal}
+var basic_element = getPageElement('basic_filter_form' + '1');
+if ((isElementVisible(basic_element) && isElementVisible(custom_filter_element)) || (!isElementVisible(basic_element) && !isElementVisible(custom_filter_element))) {
+	toggleVisibility('basic_filter_form');
+} 
+{/literal}
+-->
+</script>
---
+++ templates/en/js/my_fez.tpl.js
@@ -0,0 +1,81 @@
+<script type="text/javascript" language="javascript">
+<!--
+var page_url = '{$page_url}';
+var last_page = {if $items_info.last_page != ""}{$items_info.last_page}{else}0{/if};
+{literal}
+function checkDeleteRecords(f)
+{
+    if (!hasOneChecked(f, 'pids[]')) {
+        alert('Please select at least one item.');
+        return false;
+    }
+    if (!confirm('The checked items will be deleted, are you sure?')) {
+        return false;
+    } else {
+        var features = 'width=420,height=200,top=30,left=30,resizable=no,scrollbars=yes,toolbar=no,location=no,menubar=no,status=no';
+        var popupWin = window.open('', '_popup', features);
+        popupWin.focus();
+         return true;
+    }
+}
+function checkPublishRecords(f)
+{
+    if (!hasOneChecked(f, 'pids[]')) {
+        alert('Please select at least one item.');
+        return false;
+    }
+    if (!confirm('The checked items will be published, are you sure?')) {
+        return false;
+    } else {
+	  	
+        cat = getFormElement(f, 'cat');
+        cat.value = 'publish_objects';  
+        var features = 'width=420,height=200,top=30,left=30,resizable=no,scrollbars=yes,toolbar=no,location=no,menubar=no,status=no';
+        var popupWin = window.open('', '_popup', features);
+        popupWin.focus();
+         return true;
+    }
+}
+
+
+function checkPageField(ev)
+{
+    // check if the user is trying to submit the form by hitting <enter>
+    if (((window.event) && (window.event.keyCode == 13)) ||
+            ((ev) && (ev.which == 13))) {
+        return false;
+    }
+}
+
+function disableFields(f)
+{
+    if (current_page == 0) {
+        f.first.disabled = true;
+        f.previous.disabled = true;
+    }
+    if ((current_page == last_page) || (last_page <= 0)) {
+        f.next.disabled = true;
+        f.last.disabled = true;
+    }
+    if ((current_page == 0) && (last_page <= 0)) {
+        f.page.disabled = true;
+        f.go.disabled = true;
+    }
+}
+
+function sortList(f, field) {
+    sort_by = getSelectedOption(f, field);
+    sort_by_dir = getSelectedOption(f, 'sort_by_dir');    
+    temp = page_url + "?" + 'sort_by=' + sort_by;
+    window.location.href = temp + "&" + 'sort_by_dir=' + sort_by_dir;
+}
+
+function resizePagerMyFez(f, page_url)
+{
+    var pagesize = f.page_size.options[f.page_size.selectedIndex].value;   
+    window.location.href = page_url + "&rows=" + pagesize + "&pager_row=0";
+}
+
+{/literal}
+// -->
+</script>
