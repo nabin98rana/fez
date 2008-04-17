498fb56707f204c87e65dae59fd0ad8dda6105c5
Rhys Palmer <r.palmer@library.uq.edu.au>
2008-04-17 03:20:57 +0000

    Added Parent Publication search key and Convener search key. Related to issue #312306 and #327180

---
+++ upgrade/sql_scripts/upgrade2008041700.sql
@@ -0,0 +1,1 @@
+ALTER TABLE %TABLE_PREFIX%fulltext_cache ADD COLUMN ftc_is_text_usable TINYINT(1);
---
+++ upgrade/sql_scripts/upgrade2008041701.sql
@@ -0,0 +1,2 @@
+INSERT INTO %TABLE_PREFIX%search_key VALUES ('core_78','core',78,'Parent Publication','',0,0,0,999,'text','none','',450005,NULL,'varchar',1,'');
+INSERT INTO %TABLE_PREFIX%search_key VALUES ('core_79','core',79,'Convener','',0,0,0,999,'text','none','',450005,NULL,'varchar',1,''); 
