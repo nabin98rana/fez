01cdc5b0aaf6fdd53873e95d60d82a8392d29194
Lachlan Kuhn <lachlan@uq.edu.au>
2008-09-23 03:04:35 +0000

    - Added new core search key for Scopus ID. - Relates to issue #352057.

---
+++ upgrade/sql_scripts/upgrade2008092300.sql
@@ -0,0 +1,1 @@
+REPLACE INTO %TABLE_PREFIX%search_key VALUES ('core_84','core',84,'Scopus ID','',0,0,0,999,'text','none','',450005,NULL,'varchar',1,'',0,'','',0);
---
+++ upgrade/sql_scripts/upgrade2008092301.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `%TABLE_PREFIX%record_search_key_scopus_id` ( 
+     `rek_scopus_id_id` int(11) NOT NULL auto_increment, 
+     `rek_scopus_id_pid` varchar(64) default NULL, 
+     `rek_scopus_id_xsdmf_id` int(11) default NULL,
+      `rek_scopus_id` varchar(255) default NULL, 
+     PRIMARY KEY (`rek_scopus_id_id`), 
+     KEY `rek_scopus_id` (`rek_scopus_id`), 
+     KEY `rek_scopus_id_pid` (`rek_scopus_id_pid`) 
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
