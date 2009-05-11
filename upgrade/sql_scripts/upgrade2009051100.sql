fb909d82e1b5d431f335acfb7f6d3d53f7079a5d
Marko Tsoi <m.tsoi@library.uq.edu.au>
2009-05-11 00:53:05 +0000

    Fez statistics changes - added the table changes forgotten in previous commit - relates to issue #387590

---
+++ upgrade/sql_scripts/upgrade2009051100.sql
@@ -0,0 +1,46 @@
+create table %TABLE_PREFIX%statistics_sum_yearmonth
+(
+	sym_year char(4) not null,
+	sym_month char(2) not null,
+	sym_pid varchar(64) not null,
+	sym_title varchar(255) not null,
+	sym_downloads int not null,
+	sym_citation text,
+	primary key(sym_year, sym_month, sym_pid)
+);
+
+create table %TABLE_PREFIX%statistics_sum_countryregion
+(
+	scr_id int not null primary key auto_increment,
+	scr_country_name varchar(50),
+	scr_country_code varchar(4),
+	scr_country_region varchar(50),
+	scr_city varchar(255),
+	scr_count_abstract int,
+	scr_count_downloads int
+);
+
+create table %TABLE_PREFIX%statistics_sum_authors
+(
+	sau_author_id int not null primary key,
+	sau_author_name varchar(255),
+	sau_downloads int
+);
+
+
+create table %TABLE_PREFIX%statistics_sum_papers
+(
+	spa_pid varchar(64) not null primary key,
+	spa_title varchar(255) not null,
+	spa_citation text not null,
+	spa_downloads int not null
+);
+
+create table %TABLE_PREFIX%statistics_sum_4weeks
+(
+	s4w_pid varchar(64) not null primary key,
+	s4w_title varchar(255) not null,
+	s4w_citation text not null,
+	s4w_downloads int not null
+);
+
