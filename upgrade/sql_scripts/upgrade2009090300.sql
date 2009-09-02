bf0600b65441a20c22c27f3e2ea552be0faeca53
Christiaan Kortekaas <ck@uq.edu.au>
2009-09-02 23:35:45 +0000

    - *REQUIRES SQL UPGRADE* for new solr slave host and slave read config values - Added fez config values for solr slave host and read solr replication/load balancing as an option - Relates mainly to issue #425816

---
+++ upgrade/sql_scripts/upgrade2009090300.sql
@@ -0,0 +1,2 @@
+INSERT INTO `%TABLE_PREFIX%config` (`config_name`,`config_module`,`config_value`) VALUES ('app_solr_slave_read', 'core', 'OFF');
+INSERT INTO `%TABLE_PREFIX%config` (`config_name`,`config_module`,`config_value`) VALUES ('app_solr_slave_host', 'core', '');
