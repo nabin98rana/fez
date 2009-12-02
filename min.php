949d142c18cc6cb1288cfdaafd81a988ac17f08d
Andrew Martlew <a.martlew@library.uq.edu.au>
2009-12-02 07:46:27 +0000

    - Added a custom source to the minify script to locally cache the shib WAYF javascript - Updated the login form to use the minified copy of the WAYF js - relates to issue #461571

--- min.php
+++ min.php
@@ -8,7 +8,6 @@ set_include_path(
     . PATH_SEPARATOR . get_include_path()
 );
 
-
 $file = substr($_SERVER['PATH_INFO'], 1);
 $file = str_replace('..', '', $file);
 
@@ -18,6 +17,17 @@ if(! preg_match('/^(js\/)?([a-zA-Z0-9_\-\/\.])+.js(\?*.?)$/', $file) ) {
 }
 
 require 'Minify.php';
+
+$shib_source = array();
+if(SHIB_SWITCH == 'ON' && SHIB_VERSION == '2') {
+	$shib_source[] = new Minify_Source(array(
+	    'id' 				=> 'js/shib.js',
+	    'getContentFunc' 	=> 'shib_wayf_js_fetch',
+	    'contentType' => Minify::TYPE_JS,
+		'lastModified' => ($_SERVER['REQUEST_TIME'] - $_SERVER['REQUEST_TIME'] % 3600), // cache for 1 hour
+	));
+}
+
 Minify::setCache();
 Minify::serve('Groups', array(
     'groups' => array(
@@ -27,6 +37,20 @@ Minify::serve('Groups', array(
         'tabs.js'       =>  array(APP_PATH . '/js/tabcontent.js', APP_PATH .'/js/ajaxtabs.js'),
 		'js/editmeta.js'   =>  array(APP_PATH . '/js/editmetadata.js'),
         'js/common.js'     =>  array(APP_PATH . '/js/browserSniffer.js', APP_PATH .'/js/global.js', APP_PATH .'/js/validation.js'),
-        'js/tabs.js'       =>  array(APP_PATH . '/js/tabcontent.js', APP_PATH .'/js/ajaxtabs.js')
+        'js/tabs.js'       =>  array(APP_PATH . '/js/tabcontent.js', APP_PATH .'/js/ajaxtabs.js'),
+		'js/shib.js'       =>  $shib_source,		
     )
 ));
+
+
+function shib_wayf_js_fetch() {
+	$cache_file = APP_TEMP_DIR . '/shib_wayf_js_fetch_cache.js';
+	$js = Misc::processURL(SHIB_WAYF_JS);
+	if(is_array($js) && count($js) > 0 && (!empty($js[0])) && $js[1]['http_code'] == '200') {
+		@file_put_contents($cache_file, $js[0]);
+		return $js[0];
+	}
+	else {
+		return @file_get_contents($cache_file);
+	}
+}
