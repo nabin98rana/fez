14282d53ab098bf8685d76cfcbfad7e399fbacfb
Lachlan Kuhn <lachlan@uq.edu.au>
2010-01-28 03:36:12 +0000

    - **** REQUIRES SQL UPGRADE **** - Added new 'Language maintenance' section to administration area. - Upgrade for creating languages table + populating it. - Relates to issue #474073.

--- include/class.language.php
+++ include/class.language.php
@@ -75,4 +75,197 @@ class Language
 		}
 		@define("APP_CURRENT_LANG", $app_lang);
 	}
+	
+	
+	
+	/**
+	 * Method used to get the list of languages available in the system.
+	 *
+	 * @access  public
+	 * @return  array The list of languages
+	 */
+	function getList()
+	{
+		$log = FezLog::get();
+		$db = DB_API::get();
+
+		$stmt = "
+					SELECT
+						*
+					FROM
+						" . APP_TABLE_PREFIX . "language
+					ORDER BY
+						lng_english_name ASC
+		";
+		
+		try {
+			$res = $db->fetchAll($stmt);
+		}
+		catch(Exception $ex) {
+			$log->err($ex);
+			return '';
+		}
+		
+		return $res;
+	}	
+	
+	
+	
+	/**
+	 * Method used to get the details for a given language code.
+	 *
+	 * @access  public
+	 * @param   string $lng_code The language code
+	 * @return  array The language details
+	 */
+	function getDetails($lng_code)
+	{
+		$log = FezLog::get();
+		$db = DB_API::get();
+
+		if(empty($lng_code)) {
+			return "";
+		}
+
+		$stmt = "SELECT
+                    *
+                 FROM
+                    " . APP_TABLE_PREFIX . "language
+                 WHERE
+                    lng_alpha3_bibliographic = " . $db->quote($lng_code, 'STRING');
+        
+		try {
+			$res = $db->fetchRow($stmt);
+		}
+		catch(Exception $ex) {
+			$log->err($ex);
+			return '';
+		}
+
+		return $res;
+	}
+	
+	
+	
+	/**
+	 * Method used to update the details of the language.
+	 *
+	 * @access  public
+	 * @return  integer 1 if the update worked, -1 otherwise
+	 */
+	function update()
+	{
+		$log = FezLog::get();
+		$db = DB_API::get();
+
+		if (Validation::isWhitespace($_POST["en_name"])) {
+			return -2;
+		} elseif ($_POST["alpha3_ter"] != '' && strlen($_POST["alpha3_ter"]) != 3) {
+			return -3;
+		}  elseif ($_POST["alpha2"] != '' && strlen($_POST["alpha2"]) != 2) {
+			return -4;
+		} elseif ($_POST["ascl"] != '' && strlen($_POST["ascl"]) != 4) {
+			return -5;
+		}
+		
+		$stmt = "UPDATE
+                    " . APP_TABLE_PREFIX . "language
+                 SET
+                    lng_english_name=" . $db->quote($_POST["en_name"]) . ",
+                    lng_french_name=" . $db->quote($_POST["fr_name"]) . ",
+                    lng_alpha3_terminologic=" . $db->quote($_POST["alpha3_ter"]) . ",
+                    lng_alpha2=" . $db->quote($_POST["alpha2"]) . ",
+                    lng_ascl_code=" . $db->quote($_POST["ascl"]) . " ";
+		$stmt .= "WHERE
+                    lng_alpha3_bibliographic=" . $db->quote($_POST["id"], 'STRING');
+                    
+		try {
+			$db->exec($stmt);
+		}
+		catch(Exception $ex) {
+			$log->err($ex);
+			return -1;
+		}
+		return 1;
+	}
+	
+	
+	
+	/**
+	 * Method used to remove a given set of languages from the system.
+	 *
+	 * @access  public
+	 * @return  boolean
+	 */
+	function remove()
+	{
+		$log = FezLog::get();
+		$db = DB_API::get();
+
+		$stmt = "DELETE FROM
+                    " . APP_TABLE_PREFIX . "language
+                 WHERE
+                    lng_alpha3_bibliographic IN (".Misc::arrayToSQLBindStr($_POST["items"]).")";
+		try {
+			$db->query($stmt, $_POST['items']);
+		}
+		catch(Exception $ex) {
+			$log->err($ex);
+			return false;
+		}
+		return true;
+	}
+	
+	
+	
+	/**
+	 * Method used to add a new language to the system.
+	 *
+	 * @access  public
+	 * @return  integer 1 if the update worked, -1 or -2 otherwise
+	 */
+	function insert()
+	{
+		$log = FezLog::get();
+		$db = DB_API::get();
+
+		if (Validation::isWhitespace($_POST["en_name"])) {
+			return -2;
+		} elseif ($_POST["alpha3_ter"] != '' && strlen($_POST["alpha3_ter"]) != 3) {
+			return -3;
+		}  elseif ($_POST["alpha2"] != '' && strlen($_POST["alpha2"]) != 2) {
+			return -4;
+		} elseif ($_POST["ascl"] != '' && strlen($_POST["ascl"]) != 4) {
+			return -5;
+		}
+		
+		$stmt = "INSERT INTO
+                    " . APP_TABLE_PREFIX . "language
+                 (
+                    lng_alpha3_bibliographic,
+					lng_alpha3_terminologic,
+					lng_alpha2,
+                    lng_english_name,
+                    lng_french_name,
+                    lng_ascl_code
+                 ) VALUES (
+                    " . $db->quote($_POST["alpha3_bib"]) . ",
+					" . $db->quote($_POST["alpha3_ter"]) . ",					
+					" . $db->quote($_POST["alpha2"]) . ",
+					" . $db->quote($_POST["en_name"]) . ",
+					" . $db->quote($_POST["fr_name"]) . ",
+					" . $db->quote($_POST["ascl"]) . "
+				 )
+                  ";
+		
+		try {
+			$db->exec($stmt);
+		}
+		catch(Exception $ex) {
+			$log->err($ex);
+			return -1;
+		}
+		return 1;
+	}
+
 }
