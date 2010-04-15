e3c7740e2e12941d1593c38454dcb5ef96c2b20b
Christiaan Kortekaas <ck@uq.edu.au>
2010-04-15 04:00:32 +0000

    - created a script to import subtypes from the liaison librarian excel file for ERA - relates to issue #552470

---
+++ misc/update_subtype_from_adhoc.php
@@ -0,0 +1,86 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+// +----------------------------------------------------------------------+
+// | Fez - Digital Repository System                                      |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2005-2010 The University of Queensland,                |
+// | Australian Partnership for Sustainable Repositories,                 |
+// | eScholarship Project                                                 |
+// |                                                                      |
+// | Some of the Fez code was derived from Eventum (Copyright 2003, 2004  |
+// | MySQL AB - http://dev.mysql.com/downloads/other/eventum/ - GPL)      |
+// |                                                                      |
+// | This program is free software; you can redistribute it and/or modify |
+// | it under the terms of the GNU General Public License as published by |
+// | the Free Software Foundation; either version 2 of the License, or    |
+// | (at your option) any later version.                                  |
+// |                                                                      |
+// | This program is distributed in the hope that it will be useful,      |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+// | GNU General Public License for more details.                         |
+// |                                                                      |
+// | You should have received a copy of the GNU General Public License    |
+// | along with this program; if not, write to:                           |
+// |                                                                      |
+// | Free Software Foundation, Inc.                                       |
+// | 59 Temple Place - Suite 330                                          |
+// | Boston, MA 02111-1307, USA.                                          |
+// +----------------------------------------------------------------------+
+// | Authors: Christiaan Kortekaas <c.kortekaas@library.uq.edu.au>        |
+// +----------------------------------------------------------------------+
+ini_set("display_errors", 1); // LKDB - tmp (was 1)
+include_once('../config.inc.php');
+include_once(APP_INC_PATH . "class.record.php");
+
+$max = 100; 		// Max number of primary key IDs to send with each ESTI Search Service request call
+/*$filter = array();
+$filter["searchKey".Search_Key::getID("Status")] = 2; // enforce published records only
+$filter["searchKey".Search_Key::getID("Object Type")] = 3; // records only
+*/
+//$listing = Record::getListing(array(), array(9,10), 0, $max, 'Created Date', false, false, $filter);
+
+$query1 = 'SELECT count(*) as subtype_count FROM __era_subtype_manual_cleanup INNER JOIN ' . APP_TABLE_PREFIX . 'record_search_key on rek_pid = st_pid ';
+echo $query1."\n";
+ob_flush();
+$db = DB_API::get();
+$log = FezLog::get();
+
+try {
+        $total = $db->fetchOne($query1);
+} catch (Exception $ex) {
+        $log = FezLog::get();
+        $log->err('Message: '.$ex->getMessage().', File: '.__FILE__.', Line: '.__LINE__);
+        return;
+}
+echo "count $total subtype count for updating \n";
+$inc = 100;
+
+	ob_flush();
+for($i=0; $i<($total+$inc); $i=$i+$inc) {
+	
+	$query2 = "SELECT * FROM __era_subtype_manual_cleanup INNER JOIN " . APP_TABLE_PREFIX . "record_search_key on rek_pid
+ = st_pid ORDER BY st_pid ASC  LIMIT ".$inc." OFFSET ".$i;
+
+	echo $query2 ."\n";
+	ob_flush();
+	try {
+	        $listing = $db->fetchAll($query2);
+	} catch (Exception $ex) {
+	        $log = FezLog::get();
+	        $log->err('Message: '.$ex->getMessage().', File: '.__FILE__.', Line: '.__LINE__);
+	        return;
+	}
+	
+	if (is_array($listing)) {
+	 	for($j=0; $j<count($listing); $j++) {
+	 		$pid = $listing[$j]['st_pid'];
+	 		$subtype = $listing[$j]['st_subtype'];
+			$record = new RecordGeneral($pid);
+			$search_keys = array("Subtype");
+			$values = array($subtype);
+			echo "about to modify $pid with subtype ".$subtype."\n";
+			$record->addSearchKeyValueList("MODS", "Metadata Object Description Schema", $search_keys, $values, true);
+	 	}
+	}
+}
