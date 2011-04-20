a43b18fd4d3fd4c6348011bb4e3000afa97fb107
Christiaan Kortekaas <ck@uq.edu.au>
2011-04-20 15:53:11 +1000

    Changed SQL create table so that it doesnt give a key too long error #bing burnout revenge

--- upgrade/sql_scripts/upgrade2011042000.sql
+++ upgrade/sql_scripts/upgrade2011042000.sql
@@ -1,5 +1,5 @@
 CREATE TABLE %TABLE_PREFIX%datastream_cache (
   dc_pid varchar(64) NOT NULL,
-  dc_dsid varchar(1000) NOT NULL,
+  dc_dsid varchar(255) NOT NULL,
   PRIMARY KEY (dc_pid,dc_dsid)
 );
