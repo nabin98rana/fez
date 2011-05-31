4f32bdeb6ba42088e0abfc42e5cd80778819386d
Chris Maj <c.maj@library.uq.edu.au>
2011-05-31 10:57:59 +1000

    Background process and xml workflow for generating bulk bookreader images.

---
+++ workflow/bulk_generate_bookreader_images.php
@@ -0,0 +1,50 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+// +----------------------------------------------------------------------+
+// | Fez - Digital Repository System                                      |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2005, 2006, 2007 The University of Queensland,         |
+// | Australian Partnership for Sustainable Repositories,                 |
+// | eScholarship Project                                                 |
+// |                                                                      |
+// | Some of the Fez code was derived from Eventum (Copyright 2003, 2004  |
+// | MySQL AB - http://dev.mysql.com/downloads/other/eventum/ - GPL)      |
+// |                                                                      |
+// | This program is free software; you can redistribute it and/or modify |
+// | it under the terms of the GNU General Public License as published by |
+// | the Free Software Foundation; either version 2 of the License, or    |
+// | (at your option) any later version.                                  |
+// |                                                                      |
+// | This program is distributed in the hope that it will be useful,      |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+// | GNU General Public License for more details.                         |
+// |                                                                      |
+// | You should have received a copy of the GNU General Public License    |
+// | along with this program; if not, write to:                           |
+// |                                                                      |
+// | Free Software Foundation, Inc.                                       |
+// | 59 Temple Place - Suite 330                                          |
+// | Boston, MA 02111-1307, USA.                                          |
+// +----------------------------------------------------------------------+
+// | Authors: Christiaan Kortekaas <c.kortekaas@library.uq.edu.au>,       |
+// |          Lachlan Kuhn <l.kuhn@library.uq.edu.au>,                    |
+// |          Rhys Palmer <r.rpalmer@library.uq.edu.au>                   |
+// +----------------------------------------------------------------------+
+
+file_put_contents('/var/www/fez/pres.txt', time()."\n", FILE_APPEND);
+
+include_once(dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR."config.inc.php");
+include_once(APP_INC_PATH. 'class.bgp_bulk_generate_bookreader_images.php');
+include_once(APP_INC_PATH. 'class.error_handler.php');
+
+$pids = $this->pids;
+
+if (!empty($pids) && is_array($pids)) { 
+    
+    $bgp = new BackgroundProcess_Bulk_Generate_Bookreader_Images();
+    $bgp->register(serialize(compact('pids')), Auth::getUserID());
+    
+}
+
+?>
