54d04a36e4f05f4f1351a9844e05aff98ea9c884
Chris Maj <c.maj@library.uq.edu.au>
2011-06-07 17:00:20 +1000

    Added affiliate authors to spyglass rollover effect at the top of the view page.

--- view2.php
+++ view2.php
@@ -46,6 +46,7 @@ include_once(APP_INC_PATH . "class.user_comments.php");
 include_once(APP_INC_PATH . "class.origami.php");
 include_once(APP_PEAR_PATH . "Date.php");
 include_once(APP_INC_PATH . "class.bookreaderimplementation.php");
+include_once(APP_INC_PATH . "class.author_affiliations.php");
 
 $username = Auth::getUsername();
 $isAdministrator = Auth::isAdministrator(); 
@@ -65,8 +66,11 @@ if ($isAdministrator) {
 
 $spyglasshref = ($isSuperAdministrator) ? $get_url : '#';
 $spyglassclick = ($isSuperAdministrator) ? "javascript:window.open('$get_url'); return false;" : "";
+$affilliations = AuthorAffiliations::getListAll($pid);
+//die(var_dump($affilliations[0]));
 $tpl->assign('spyglasshref', $spyglasshref);
 $tpl->assign('spyglassclick', $spyglassclick);
+$tpl->assign('affilliations', $affilliations);
 
 $tpl->assign("fez_root_dir", APP_PATH);
 $tpl->assign("eserv_url", APP_BASE_URL."eserv/".$pid."/");
