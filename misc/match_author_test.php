a41f633e1d801419d2685a4a3758c51a5d3d45a9
Lachlan Kuhn <lachlan@uq.edu.au>
2011-06-20 11:09:57 +1000

    - Added some espace branch misc scripts back to trunk. - Relates to issue #725399 - #bing christiaan kortekaas naked

---
+++ misc/match_author_test.php
@@ -0,0 +1,8 @@
+<?php
+include_once(dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR."config.inc.php");
+include_once(APP_INC_PATH . "class.record_general.php");
+echo " here";
+$rec = new RecordGeneral('UQ:240823');
+echo " here";
+$rec->matchAuthor(80900, TRUE, TRUE);
+
