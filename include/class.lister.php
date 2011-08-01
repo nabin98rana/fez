36953a540f19e90caaa44d1fb3505811c28ffea1
Joseph Heaphey <joseph.heaphey@deakin.edu.au>
2011-08-01 10:11:10 +1000

    eliminate extra pager_row var appearing in the url when paging through a collection #bing wayne shorter

--- include/class.lister.php
+++ include/class.lister.php
@@ -1071,6 +1071,7 @@ class Lister
         if(count($params) > 0) {
             
             $exclude[] = 'rows';
+            $exclude[] = 'pager_row';
             $tpl->assign('url_wo_rows', Misc::query_string_encode($params,$exclude));
             array_pop($exclude);
             
