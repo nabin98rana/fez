6efd445e6c5f565f636d12225d31c48b912925c8
Aaron Brown <a.brown@library.uq.edu.au>
2011-10-20 14:08:39 +1000

    Add in 3 config vars to do with Collections
    https://www.pivotaltracker.com/story/show/19870039

---
+++ upgrade/sql_scripts/upgrade2011102000.sql
@@ -0,0 +1,3 @@
+insert ignore into %TABLE_PREFIX%config (config_name, config_module, config_value) values ('app_portfolios_collection','core','');
+insert ignore into %TABLE_PREFIX%config (config_name, config_module, config_value) values ('app_individual_nto_collection','core','');
+insert ignore into %TABLE_PREFIX%config (config_name, config_module, config_value) values ('app_era_whitelist_collection','core','');
