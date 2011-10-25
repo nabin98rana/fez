3f102907502541ca781d1efeaf05e8b2cda71073
Christiaan Kortekaas <ck@uq.edu.au>
2011-10-25 10:28:28 +1000

    Changed tombstone table creation upgrade script to not have a redundant fez_ prefix and for the pid columns to be varchar 64 like the other pid columns in other tables in Fez relates to story https://www.pivotaltracker.com/story/show/17616783 #bing tombstone

--- upgrade/sql_scripts/upgrade2011102100.sql
+++ upgrade/sql_scripts/upgrade2011102100.sql
@@ -1,7 +1,7 @@
-CREATE TABLE IF NOT EXISTS %TABLE_PREFIX%fez_tombstone (
+CREATE TABLE IF NOT EXISTS %TABLE_PREFIX%tombstone (
   `tom_id` int(11) NOT NULL AUTO_INCREMENT,
-  `tom_pid_main` varchar(45) DEFAULT NULL,
-  `tom_pid_rel` varchar(45) DEFAULT NULL,
+  `tom_pid_main` varchar(64) DEFAULT NULL,
+  `tom_pid_rel` varchar(64) DEFAULT NULL,
   `tom_delete_ts` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`tom_id`)
 );
