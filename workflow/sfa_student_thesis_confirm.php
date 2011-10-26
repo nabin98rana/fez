a30916f4b63631437687b637ec416d6484778842
Elvi Shu <e.shu@library.uq.edu.au>
2011-10-26 11:27:39 +1000

    Added full metadata and uploaded files for Student and Post-Doctorate Thesis submission on the confirmation pages & receipt emails. Utilised a PHP class that responsible in preparing / cleaning the display data that used on the template level. Separated the template files for more sustainable code base. Relates to story: https://www.pivotaltracker.com/story/show/18453609
    #bing thesis

--- workflow/sfa_student_thesis_confirm.php
+++ workflow/sfa_student_thesis_confirm.php
@@ -36,8 +36,11 @@ include_once(dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR."config.inc.php");
 include_once(APP_INC_PATH . "class.template.php");
 include_once(APP_INC_PATH . "class.auth.php");
 
+
+// Authentication check
 Auth::checkAuthentication(APP_SESSION, $_SERVER['PHP_SELF']."?".$_SERVER['QUERY_STRING']);
 
+// Checks Workflow status
 $wfstatus = &WorkflowStatusStatic::getSession(); // restores WorkflowStatus object from the session
 $wfstatus->checkStateChange();
 if (empty($wfstatus)) {
@@ -45,37 +48,55 @@ if (empty($wfstatus)) {
     exit;
 }
 
+// Get PID
 $pid = $wfstatus->pid;
-$record = new RecordObject($pid);
-$record->getObjectAdminMD();
 
-$view_record_url = 'http://'.APP_HOSTNAME.APP_RELATIVE_URL."view/".$pid;
-if (Misc::isValidPid($pid)) {
-    if ($record->isCommunity()) {
-        $view_record_url = APP_RELATIVE_URL."community/".$pid;
-    } elseif ($record->isCollection()) {
-        $view_record_url = APP_RELATIVE_URL."collection/".$pid;
-    }
-    $record_title = $record->getTitle();
-} else {
-    $record_title = $pid;
-}
+// Utilising Fez_Workflow_Sfa_Confirm class to produce a clean metadata that we can use on the template
+// Instantiate Confirm class
+$confirmation = new Fez_Workflow_Sfa_Confirm($pid);
 
-if(is_numeric($record->depositor)) {
-	$usrDetails = User::getDetailsByID($record->depositor);
-	
+// Get display data to be used by smarty template
+$display_data = $confirmation->getDisplayData();
+
+
+// Assigns the URL for viewing the thesis' record
+$view_record_url = $confirmation->getViewURL();
+
+// Assigns the record title
+$record_title = $confirmation->getRecordTitle();
+
+$usrDetails = User::getDetailsByID($confirmation->record->depositor);
+
+// Submission confirmation email
+if(is_numeric($confirmation->record->depositor)) {
+
+    // Plain text email content
 	$tplEmail = new Template_API();
 	$tplEmail->setTemplate('workflow/emails/sfa_student_thesis_confirm.tpl.txt');
 	$tplEmail->assign('application_name', APP_NAME);
-	$tplEmail->assign('title', $record->getTitle());
+	$tplEmail->assign('title', $record_title);
 	$tplEmail->assign('name', $usrDetails['usr_full_name']);
 	$tplEmail->assign('view_record_url', $view_record_url);
-	
+    $tplEmail->assign("display_data", $display_data);
+
 	$email_txt = $tplEmail->getTemplateContents();
-	
-	
+
+    // HTML based email content
+	$tplEmailHTML = new Template_API();
+	$tplEmailHTML->setTemplate('workflow/emails/sfa_student_thesis_confirm.tpl.html');
+	$tplEmailHTML->assign('application_name', APP_NAME);
+	$tplEmailHTML->assign('title', $record_title);
+	$tplEmailHTML->assign('name', $usrDetails['usr_full_name']);
+	$tplEmailHTML->assign('view_record_url', $view_record_url);
+    $tplEmailHTML->assign("display_data", $display_data);
+
+	$email_html = $tplEmailHTML->getTemplateContents();
+
+
 	$mail = new Mail_API;
 	$mail->setTextBody(stripslashes($email_txt));
+	$mail->setHTMLBody(stripslashes($email_html));
+
 	$subject = '['.APP_NAME.'] - Your submission has been completed';
 	$from = APP_EMAIL_SYSTEM_FROM_ADDRESS;
 	$to = $usrDetails['usr_email'];
@@ -83,17 +104,18 @@ if(is_numeric($record->depositor)) {
 	$mail->send($from, $to, $subject, false);
 }
 
+// Display Submission confirmation
 $tpl = new Template_API();
 $tpl->setTemplate("workflow/index.tpl.html");
 $tpl->assign("type", 'sfa_student_thesis_confirm');
+$tpl->assign('application_name', APP_NAME);
 $tpl->assign('view_record_url', $view_record_url);
 $tpl->assign('record_title', $record_title);
-$tpl->assign('application_name', APP_NAME);
-$tpl->assign('title', $record->getTitle());
+$tpl->assign('title', $record_title);
 $tpl->assign('name', $usrDetails['usr_full_name']);
+$tpl->assign("display_data", $display_data);
 
 $tpl->displayTemplate();
 
 // This is a special ending workflow state -> so end the workflow manually rather than goto the redirect screen (to prevent users clicking back the browser and causing all sorts of trouble)
 $wfstatus->theend(false);
-?>
