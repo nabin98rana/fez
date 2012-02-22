420e59f88afac400c578d0de53a30f8aadedb650
Elvi Shu <e.shu@library.uq.edu.au>
2012-02-23 09:58:39 +1000

    ** REQUIRES SQL UPGRADE ** Alter BLOB typed columns to MEDIUMBLOB on RID jobs tables. Relates to story: https://www.pivotaltracker.com/story/show/25359835 #bing blob

---
+++ upgrade/sql_scripts/upgrade2012022300.sql
@@ -0,0 +1,12 @@
+ALTER TABLE %TABLE_PREFIX%rid_profile_uploads 
+CHANGE `rpu_response` `rpu_response` MEDIUMBLOB;
+
+ALTER TABLE %TABLE_PREFIX%rid_jobs 
+CHANGE `rij_response_profilexml` `rij_response_profilexml` MEDIUMBLOB;
+
+ALTER TABLE %TABLE_PREFIX%rid_jobs 
+CHANGE `rij_response_publicationsxml` `rij_response_publicationsxml` MEDIUMBLOB;
+
+ALTER TABLE %TABLE_PREFIX%rid_registration
+CHANGE `rre_response` `rre_response` MEDIUMBLOB;
+
