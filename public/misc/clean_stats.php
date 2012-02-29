a3911b6379a4ceb055bd988c6627804fd3a6530c
Aaron Brown <a.brown@library.uq.edu.au>
2012-02-29 13:31:46 +1000

    Standardised misc includes Increased convert audio and video processURL timeouts Relates to story https://www.pivotaltracker.com/story/show/15749439

--- public/misc/clean_stats.php
+++ public/misc/clean_stats.php
@@ -1,6 +1,6 @@
 <?php
 include_once dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR.'config.inc.php';
-include_once("/var/www/fez/include/class.statistics.php");
+include_once APP_INC_PATH.'class.statistics.php';
 Statistics::cleanupFalseHits();
 //print_r($GLOBALS);
 //session_start();
