9dc84aa25851ea502f907e341331afbcec3547f8
Elvi Shu <e.shu@library.uq.edu.au>
2012-03-19 14:14:36 +1000

    Fixed incorrect destination table name for rek_security_inherited column, from file_attachment to record_search_key Relates to story: https://www.pivotaltracker.com/story/show/26059095 #bing table tennis

--- public/upgrade/sql_scripts/upgrade2012022101.sql
+++ public/upgrade/sql_scripts/upgrade2012022101.sql
@@ -1,1 +1,1 @@
-ALTER TABLE %TABLE_PREFIX%file_attachments ADD COLUMN rek_security_inherited tinyint(4) NULL AFTER rek_wok_doc_type; 
+ALTER TABLE %TABLE_PREFIX%record_search_key ADD COLUMN rek_security_inherited tinyint(4) NULL AFTER rek_wok_doc_type; 
