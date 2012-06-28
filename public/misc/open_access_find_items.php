95875862c40aa228aa0a339198cd0ac37068c4e4
Aaron Brown <a.brown@library.uq.edu.au>
2012-06-28 10:29:26 +1000

    Misc function to find all open access items available
    Relates to story https://www.pivotaltracker.com/story/show/31194339

---
+++ public/misc/open_access_find_items.php
@@ -0,0 +1,119 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+// +----------------------------------------------------------------------+
+// | Fez - Digital Repository System                                      |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2005, 2006, 2007 The University of Queensland,         |
+// | Australian Partnership for Sustainable Repositories,                 |
+// | eScholarship Project                                                 |
+// |                                                                      |
+// | Some of the Fez code was derived from Eventum (Copyright 2003, 2004  |
+// | MySQL AB - http://dev.mysql.com/downloads/other/eventum/ - GPL)      |
+// |                                                                      |
+// | This program is free software; you can redistribute it and/or modify |
+// | it under the terms of the GNU General Public License as published by |
+// | the Free Software Foundation; either version 2 of the License, or    |
+// | (at your option) any later version.                                  |
+// |                                                                      |
+// | This program is distributed in the hope that it will be useful,      |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+// | GNU General Public License for more details.                         |
+// |                                                                      |
+// | You should have received a copy of the GNU General Public License    |
+// | along with this program; if not, write to:                           |
+// |                                                                      |
+// | Free Software Foundation, Inc.                                       |
+// | 59 Temple Place - Suite 330                                          |
+// | Boston, MA 02111-1307, USA.                                          |
+// +----------------------------------------------------------------------+
+// | Authors: Aaron Brown <a.brown@library.uq.edu.au>                    |
+// +----------------------------------------------------------------------+
+//
+// This function finds open access(Viewable while not logged in) attachments and puts them into a table for further analysis
+// It has no web based interface
+/*
+ CREATE TABLE `fez_open_access_items` (
+  `oai_id` int(11) NOT NULL AUTO_INCREMENT,
+  `oai_pid` varchar(255) DEFAULT NULL,
+  `oai_dsid` varchar(255) DEFAULT NULL,
+  `oai_document_type` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`oai_id`)
+)
+
+sudo php open_access_find_items.php > /var/log/open_access_find_items.tmp
+
+*/
+
+include_once dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'config.inc.php';
+include_once(APP_INC_PATH . "class.auth.php");
+error_reporting(E_ALL & !E_NOTICE);
+
+//We'll only allow this to be run from the command line, if it's run from the web it will find all  accessiable by the person currently logged in.
+if ((php_sapi_name()==="cli")) {
+    set_time_limit(0);
+    echo "Script started: " . date('Y-m-d H:i:s') . "\n";
+
+    $db = DB_API::get();
+    $log = FezLog::get();
+
+    $query = "DELETE FROM " . APP_TABLE_PREFIX . "open_access_items";
+    try {
+        $db->exec($query);
+    }
+    catch (Exception $ex) {
+        null; // don't do anything here.
+    }
+
+    $fedoraPids = Fedora_Direct_Access::fetchAllFedoraPIDs('','');
+    //$fedoraPids[] = array('pid' => 'UQ:3875');
+    $status = Status::getID("Published");
+    foreach ($fedoraPids as $pid) {
+        $pid = $pid['pid'];
+        $documentType = Record::getSearchKeyIndexValue($pid, "Genre", false);
+        if ($status == Record::getSearchKeyIndexValue($pid, "Status", false)) {
+            $datastreams = Fedora_API::callGetDatastreams($pid);
+            foreach ($datastreams as $datastream) {
+                if ($datastream['controlGroup'] == "M"
+                    && (!Misc::hasPrefix($datastream['ID'], 'preview_')
+                        && !Misc::hasPrefix($datastream['ID'], 'web_')
+                        && !Misc::hasPrefix($datastream['ID'], 'thumbnail_')
+                        && !Misc::hasPrefix($datastream['ID'], 'stream_')
+                        && !Misc::hasPrefix($datastream['ID'], 'presmd_'))) {
+
+                    $userPIDAuthGroups = Auth::getAuthorisationGroups($pid, $datastream['ID']);
+                    if (in_array('Viewer', $userPIDAuthGroups)) {
+                        saveOpenAccessItemInfo($pid, $datastream['ID'], $documentType);
+                        $open = true;
+
+                    }
+                }
+            }
+        }
+
+        if ($open) {
+            $open = false;
+            echo "--------------------------\n";
+        }
+    }
+
+    echo "Script finished: " . date('Y-m-d H:i:s') . "\n";
+    echo "--------------------------\n";
+
+} else {
+    echo "Must be run from the command line";
+}
+
+function saveOpenAccessItemInfo($pid, $dsID, $documentType) {
+    $log = FezLog::get();
+    $db = DB_API::get();
+    $stmt = "INSERT INTO ".APP_TABLE_PREFIX."open_access_items  (oai_pid, oai_dsid, oai_document_type)
+                VALUES (".$db->quote($pid).", ".$db->quote($dsID).", ".$db->quote($documentType).")";
+    try {
+        $res = $db->exec($stmt);
+    }
+    catch(Exception $ex) {
+        echo $ex;
+    }
+    echo $pid." ". $dsID . " ". $documentType."\n";
+}
