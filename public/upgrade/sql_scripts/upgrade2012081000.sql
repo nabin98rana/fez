3a3cb114d82baf01dad5c8e9df5f8f8fca83ab53
Aaron Brown <a.brown@library.uq.edu.au>
2012-08-10 11:36:35 +1000

    SQL Upgrade for Fedora Bypass required
    https://www.pivotaltracker.com/story/show/33538421

---
+++ public/upgrade/sql_scripts/upgrade2012081000.sql
@@ -0,0 +1,15 @@
+CREATE TABLE %TABLE_PREFIX%auth_quick_rules (
+  qac_id int(11) unsigned NOT NULL DEFAULT '0',
+  qac_role int(11) DEFAULT NULL,
+  qac_arg_id int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET==utf8;
+
+CREATE TABLE %TABLE_PREFIX%auth_quick_rules_id (
+  qai_id int(11) DEFAULT NULL,
+  qai_title varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE %TABLE_PREFIX%auth_quick_rules_pid (
+  qrp_pid varchar(255) DEFAULT NULL,
+  qrp_qac_id int(11) unsigned DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
