7f3e74f67e8076cd81b34997d4e740370e3788e4
Chris Maj <c.maj@library.uq.edu.au>
2012-08-21 18:29:10 +1000

    Remove superfluous equals sign from SQL upgrade

--- public/upgrade/sql_scripts/upgrade2012081000.sql
+++ public/upgrade/sql_scripts/upgrade2012081000.sql
@@ -1,15 +1,15 @@
-CREATE TABLE %TABLE_PREFIX%auth_quick_rules (
+CREATE TABLE IF NOT EXISTS %TABLE_PREFIX%auth_quick_rules (
   qac_id int(11) unsigned NOT NULL DEFAULT '0',
   qac_role int(11) DEFAULT NULL,
   qac_arg_id int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET==utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE %TABLE_PREFIX%auth_quick_rules_id (
+CREATE TABLE IF NOT EXISTS %TABLE_PREFIX%auth_quick_rules_id (
   qai_id int(11) DEFAULT NULL,
   qai_title varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE %TABLE_PREFIX%auth_quick_rules_pid (
+CREATE TABLE IF NOT EXISTS %TABLE_PREFIX%auth_quick_rules_pid (
   qrp_pid varchar(255) DEFAULT NULL,
   qrp_qac_id int(11) unsigned DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
