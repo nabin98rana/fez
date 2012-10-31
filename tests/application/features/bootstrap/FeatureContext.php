b82ebba47cb097c9a1e045f3c240d9485ecf0016
Christiaan Kortekaas <ck@uq.edu.au>
2012-10-31 16:55:01 +1000

    Aligned Behat namespaces in fez with the drupal name spaces tests by adding ExtensionManager #bing i have no idea what I'm doing

--- tests/application/features/bootstrap/FeatureContext.php
+++ tests/application/features/bootstrap/FeatureContext.php
@@ -3,6 +3,7 @@
 use Behat\Behat\Context\ClosuredContextInterface,
     Behat\Behat\Context\TranslatedContextInterface,
     Behat\Behat\Context\BehatContext,
+    Behat\Behat\Extension\ExtensionManager,
     Behat\Behat\Exception\PendingException;
 use Behat\Gherkin\Node\PyStringNode,
     Behat\Gherkin\Node\TableNode;
