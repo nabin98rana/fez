b10af132c3be4320de683f0881f8daa8d942c2b5
Christiaan Kortekaas <ck@uq.edu.au>
2012-11-02 16:32:26 +1000

    fixed acodec vorbis -> libvorbis

--- tests/application/features/bootstrap/FeatureContext.php
+++ tests/application/features/bootstrap/FeatureContext.php
@@ -867,7 +867,7 @@ class ZoetropeBackgroundService extends BackgroundService
     // -r      Frame rate
     $command = 'ffmpeg -an -f x11grab -y -r 5 -s 1024x768 -i ' . $screenId . '.0+0,0 '
               . '-codec:v libvpx -quality good -cpu-used 0 -b:v 1200k -maxrate 1200k -bufsize 2400k -qmin 10 '
-              . '-qmax 42 -vf scale=-1:480 -threads 4 -codec:a vorbis -b:a 128k '
+              . '-qmax 42 -vf scale=-1:480 -threads 4 -codec:a libvorbis -b:a 128k '
               . $this->fileDir . $this->videoFiles['webm'] . ' '
               . '-codec:v libtheora -sameq ' . $this->fileDir . $this->videoFiles['ogg'] . ' '
               . '-codec:v libx264 -sameq ' . $this->fileDir . $this->videoFiles['mp4'];
