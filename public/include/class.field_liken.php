c66ffb9a385f703d2fcec9945bd74c6095067a44
Chris Maj <c.maj@library.uq.edu.au>
2012-11-26 13:27:51 +1000

    Start of import class abstraction.

---
+++ public/include/class.field_liken.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * Servant class to execute liken functionality
+ * for RecordItem classes
+ * @author Chris Maj <c.maj@library.uq.edu.au>
+ * @since November 2012
+ *
+ */
+class FieldLiken
+{
+    /**
+     * The ingested record
+     * @var RecordItem
+     */
+    protected $record;
+    
+    /**
+     * Liken methods to run
+     * @var array
+     */
+    protected $likenMethods = array();
+    
+    public function __construct(RecordItem $record)
+    {
+        $this->record = $record;
+    }
+    
+    /**
+     * Public interface for running methods
+     */
+    public function run()
+    {
+        $this->reflect();
+        $this->liken();
+    }
+    
+    /**
+     * Determine what comparison methods to run
+     */
+    protected function reflect()
+    {
+        $ref = new ReflectionClass($this->record);
+        $methods = $ref->getMethods();
+        
+        foreach($methods as $method)
+        {
+            if($likenMethod = preg_match("/^liken.*/", $method->name))
+            {
+                $this->likenMethods[] = $method->name;
+            }
+        }
+    }
+    
+    /**
+     * Run comparison methods
+     */
+    protected function liken()
+    {
+        foreach($this->likenMethods as $likenMethod)
+        {
+            $this->record->$likenMethod();
+        }
+    }
+}
