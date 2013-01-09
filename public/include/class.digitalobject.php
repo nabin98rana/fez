5846d354d350f2d054717c9e179c6d8dff9df724
Aaron Brown <a.brown@library.uq.edu.au>
2013-01-10 09:25:21 +1000

    Fix Check Published to make sure it works on deleted pids. Fixed some coding sstandards.

--- public/include/class.digitalobject.php
+++ public/include/class.digitalobject.php
@@ -6,37 +6,31 @@ class DigitalObject
      * The object's DB connection.
      * @var <Zend_Db>
      */
-    private $db;
+    private $_db;
 
     /**
      * Metadata for a digital object.
      * @var <array>
      */
-    private $pidData;
+    private $_pidData;
 
     /**
      * Logging object.
      * @var <FezLog>
      */
-    private $log;
+    private $_log;
 
     /**
      * The currentn PID
      * @var <string>
      */
-    private $pid;
-
-    /**
-     * A collection of this PID's loaded datastreams.
-     * @var <array>
-     */
-    private $dataStreams;
+    private $_pid;
 
     /**
      * A DSResource to handle datastreams.
      * @var <DSResource>
      */
-    private $dsResource;
+    private $_dsResource;
 
     /**
      * The next PID should be at least this
@@ -49,9 +43,9 @@ class DigitalObject
      */
     public function __construct()
     {
-        $this->db = DB_API::get();
-        $this->log = FezLog::get();
-        $this->dsResource = new DSResource();
+        $this->_db = DB_API::get();
+        $this->_log = FezLog::get();
+        $this->_dsResource = new DSResource();
     }
 
     /**
@@ -62,17 +56,15 @@ class DigitalObject
      */
     public function save($objdata)
     {
-        if(array_key_exists('pid', $objdata))
-        {
-            $this->pid = $objdata['pid'];
+        if (array_key_exists('pid', $objdata)) {
+            $this->_pid = $objdata['pid'];
 
             list($pidns, $pidint) = explode(":", $objdata['pid']);
 
             unset($objdata['pid']); //Exclude the pid from the update.
             $updateFields = $this->setUpdateFields($objdata);
 
-            try
-            {
+            try {
                 $sql = "UPDATE " . APP_TABLE_PREFIX . "digital_object SET "
                     . $updateFields['set'] . " WHERE pidns = :pidns AND pidint = :pidint";
 
@@ -80,53 +72,43 @@ class DigitalObject
                 $updateFields['binding'][':pidns'] = $pidns;
                 $updateFields['binding'][':pidint'] = $pidint;
 
-                $this->db->query($sql, $updateFields['binding']);
+                $this->_db->query($sql, $updateFields['binding']);
+            } catch(Exception $e) {
+                $this->_log->err($e->getMessage());
             }
-            catch(Exception $e)
-            {
-                $this->log->err($e->getMessage());
-            }
-        }
-        else
-        {
+        } else {
             //If no namespace, use the one in the config.
             $pidns = ($objdata['pidns']) ? $objdata['pidns'] : APP_PID_NAMESPACE;
 
-            try
-            {
+            try {
                 $sql = "SELECT MAX(pidint)+1 AS pidint FROM " . APP_TABLE_PREFIX
                     . "digital_object WHERE pidns = :pidns";
-                $stmt = $this->db->query($sql, array(':pidns' => $pidns));
+                $stmt = $this->_db->query($sql, array(':pidns' => $pidns));
                 $pidint = $stmt->fetch();
-            }
-            catch(Exception $e)
-            {
-                $this->log->err($e->getMessage());
+            } catch(Exception $e) {
+                $this->_log->err($e->getMessage());
             }
 
             //Check to see if this the first pid for this namespace.
             $pidint = ($pidint['pidint'] == NULL) ? 1 : $pidint['pidint'];
             $pidint = ($pidint < self::PID_NO_CLASH) ? self::PID_NO_CLASH : $pidint;
 
-            $this->pid = $pidns . ":" . $pidint;
+            $this->_pid = $pidns . ":" . $pidint;
 
             $objdata['pidint'] = $pidint;
             $objdata['pidns'] = $pidns;
 
             $insert = $this->setInsertFields($objdata);
 
-            try
-            {
+            try {
                 $sql = "INSERT INTO " . APP_TABLE_PREFIX . "digital_object " . $insert['insert'];
-                $this->db->query($sql, $insert['binding']);
-            }
-            catch(Exception $e)
-            {
-                $this->log->err($e->getMessage());
+                $this->_db->query($sql, $insert['binding']);
+            } catch(Exception $e) {
+                $this->_log->err($e->getMessage());
             }
         }
 
-        return $this->pid;
+        return $this->_pid;
     }
 
     /**
@@ -138,8 +120,7 @@ class DigitalObject
     {
         $set = '';
         $binding = array();
-        foreach($fields as $fieldk => $fieldv)
-        {
+        foreach ($fields as $fieldk => $fieldv) {
             $set .= $fieldk . " = :" . $fieldk . ",";
             $binding[':' . $fieldk] = $fieldv;
         }
@@ -158,8 +139,7 @@ class DigitalObject
         $tokens = array();
         $binding = array();
 
-        foreach($data as $datak => $datav)
-        {
+        foreach ($data as $datak => $datav) {
             $fields[] = $datak;
             $tokens[] = ':'.$datak;
             $binding[':'.$datak] = $datav;
@@ -178,16 +158,13 @@ class DigitalObject
      */
     public function get($pid)
     {
-        try
-        {
+        try {
             $sql = "SELECT * FROM " . APP_TABLE_PREFIX . "record_search_key WHERE"
                 . " rek_pid = :pid";
-            $stmt = $this->db->query($sql, array(':pid' => $pid));
+            $stmt = $this->_db->query($sql, array(':pid' => $pid));
             return $stmt->fetch();
-        }
-        catch(Exception $e)
-        {
-            $this->log->err($e->getMessage());
+        } catch(Exception $e) {
+            $this->_log->err($e->getMessage());
         }
     }
 
@@ -199,37 +176,9 @@ class DigitalObject
     {
         $pidData = $this->get($pid);
         $pidData['pid'] = $pid;
-        $this->pidData = $pidData;
+        $this->_pidData = $_pidData;
     }
 
-    /**
-     * Take a snapshot of the current state of
-     * all this PID's resources and store in the
-     * shadow table
-     */
-//    public function snapshotResources($timestamp)
-//    {
-//        $resources = $this->dsResource->listStreams($this->pidData['pid']);
-//
-//        if($resources)
-//        {
-//            try
-//            {
-//                $sql = "INSERT IGNORE INTO " . APP_TABLE_PREFIX . "file_attachments__shadow "
-//                        . "(fat_hash, fat_filename, fat_version, fat_state, fat_size, fat_pid, fat_mimetype, fat_controlgroup) "
-//                        . "SELECT fat_hash, fat_filename, :now AS version, fat_state, fat_size,fat_pid, fat_mimetype, "
-//                        . "fat_controlgroup FROM " . APP_TABLE_PREFIX
-//                        . "file_attachments WHERE fat_pid = :pid";
-//
-//                $this->db->query($sql, array(':now' => $timestamp,
-//                						':pid' => $this->pidData['pid']));
-//            }
-//            catch(Exception $e)
-//            {
-//                $this->log->err($e->getMessage());
-//            }
-//        }
-//    }
 
     /**
      * Get all the datastreams for a PID
@@ -237,15 +186,14 @@ class DigitalObject
      */
     public function getDatastreams($params)
     {
-        $dsList = $this->dsResource->listStreams($params['pid']);
+        $dsList = $this->_dsResource->listStreams($params['pid']);
         $datastreams = array();
 
         $rev = (isset($params['rev'])) ? $params['rev'] : 'HEAD';
 
-        foreach($dsList as $datastream)
-        {
+        foreach ($dsList as $datastream) {
             $dsFormatted = array();
-            $dsRev = $this->dsResource->getDSRev($datastream['filename'], $datastream['pid'], $rev);
+            $dsRev = $this->_dsResource->getDSRev($datastream['filename'], $datastream['pid'], $rev);
             $dsFormatted['controlGroup'] = $dsRev['controlgroup'];
             $dsFormatted['ID'] = $dsRev['filename'];
             $dsFormatted['versionID'] = $dsRev['version'];
@@ -265,19 +213,29 @@ class DigitalObject
         return $datastreams;
     }
 
+    //A record can be published but deleted. Which means the tombstone is viewable by the public.
     public function isPublished($pid)
     {
-        try
-        {
-            $sql = "SELECT rek_status FROM " . APP_TABLE_PREFIX . "record_search_key WHERE"
-                . " rek_pid = :pid";
-            $stmt = $this->db->query($sql, array(':pid' => $pid));
-            return $stmt->fetchColumn();
+        $stmt = "SELECT rek_status FROM " . APP_TABLE_PREFIX . "record_search_key WHERE"
+            . " rek_pid = ".$this->_db->quote($pid);
+
+        try {
+            $result = $this->_db->fetchOne($stmt);
+            if (empty($result)) {
+                $stmt = "SELECT rek_status FROM fez_record_search_key__shadow WHERE rek_pid = ".$this->_db->quote($pid)." ORDER BY rek_stamp DESC LIMIT 1";
+                try {
+                    $result = $this->_db->fetchOne($stmt);
+                }
+                catch(Exception $ex) {
+                    $this->_log->err($ex);
+                    return false;
+                }
+            }
+        } catch(Exception $ex) {
+            $this->_log->err($ex);
+            return false;
         }
-        catch(Exception $e)
-        {
-            $this->log->err($e->getMessage());
-        }
+        return $result;
 
     }
 
