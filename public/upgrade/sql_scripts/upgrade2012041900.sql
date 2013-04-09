9e65e2e5855cecbfdbfcf19e34e2ef04e2129ad0
Jonathan Harker <jonathan@catalyst.net.nz>
2013-04-09 16:00:48 +1200

    update/sql_scripts: Fix incorrect escaping of \ in the Scopus ID regex.

--- public/upgrade/sql_scripts/upgrade2012041900.sql
+++ public/upgrade/sql_scripts/upgrade2012041900.sql
@@ -1,7 +1,7 @@
 ALTER TABLE %TABLE_PREFIX%xsd_display_matchfields ADD xsdmf_validation_regex mediumtext AFTER xsdmf_validation_maxlength ;
 ALTER TABLE %TABLE_PREFIX%xsd_display_matchfields ADD xsdmf_validation_message mediumtext AFTER xsdmf_validation_regex ;
 
-UPDATE %TABLE_PREFIX%xsd_display_matchfields SET xsdmf_validation_regex = '/^2-s2\.0-[0-9]{10,11}$/',
+UPDATE %TABLE_PREFIX%xsd_display_matchfields SET xsdmf_validation_regex = '/^2-s2\\.0-[0-9]{10,11}$/',
 xsdmf_validation_message = 'Scopus ID must be between 17 and 18 characters in length, beginning with 2-s2.0- and ending only in numbers eg 2-s2.0-0346778587'
 WHERE xsdmf_sek_id = 'core_84';
 
