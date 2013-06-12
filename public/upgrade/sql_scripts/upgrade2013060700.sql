976e9d2bee27ffc9ace5f3fbd261baba8362870d
Christiaan Kortekaas <ck@uq.edu.au>
2013-06-12 10:46:05 +1000

    Fixed the reference to the CORRECT search key core id number in the issn/isbn sql upgrade script #bing sad face

--- public/upgrade/sql_scripts/upgrade2013060700.sql
+++ public/upgrade/sql_scripts/upgrade2013060700.sql
@@ -6,4 +6,4 @@ UPDATE %TABLE_PREFIX%search_key SET sek_cardinality = '1' WHERE sek_id = 'core_6
 ALTER TABLE %TABLE_PREFIX%record_search_key_issn ADD COLUMN rek_issn_order INT(11) DEFAULT 1 NULL AFTER rek_issn;
 CREATE UNIQUE INDEX unique_constraint_pid_order ON %TABLE_PREFIX%record_search_key_issn (rek_issn_pid, rek_issn_order);
 ALTER TABLE %TABLE_PREFIX%record_search_key_issn DROP INDEX rek_issn_pid;
-UPDATE %TABLE_PREFIX%search_key SET sek_cardinality = '1' WHERE sek_id = 'core_65';
+UPDATE %TABLE_PREFIX%search_key SET sek_cardinality = '1' WHERE sek_id = 'core_63';
