bbe1419acfd6ff86e0bb2c8d7c9f815684676aea
Aaron Brown <a.brown@library.uq.edu.au>
2013-07-04 08:50:46 +1000

    *REQUIRES SQL UPGRADE*
    https://www.pivotaltracker.com/story/show/51047147
    https://www.pivotaltracker.com/story/show/51047123

---
+++ public/upgrade/sql_scripts/upgrade2013070300.sql
@@ -0,0 +1,10 @@
+CREATE TABLE %TABLE_PREFIX%datastream_info (
+  dsi_id int(11) NOT NULL AUTO_INCREMENT,
+  dsi_pid varchar(255) DEFAULT NULL,
+  dsi_dsid varchar(255) DEFAULT NULL,
+  dsi_permissions varchar(255) DEFAULT NULL,
+  dsi_embargo_date date DEFAULT NULL,
+  PRIMARY KEY (dsi_id),
+  KEY pid, dsid (dsi_pid,dsi_dsid),
+  KEY embargo_date (dsi_embargo_date)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
