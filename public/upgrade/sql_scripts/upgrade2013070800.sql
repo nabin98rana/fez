62e0536c551304c6a4021e7b7ab53a47973b3c9f
Aaron Brown <a.brown@library.uq.edu.au>
2013-07-08 11:28:25 +1000

    - REQUIRES SQL UPGRADE - Open access tables (fix)
    https://www.pivotaltracker.com/story/show/49374145

--- public/upgrade/sql_scripts/upgrade2013070800.sql
+++ public/upgrade/sql_scripts/upgrade2013070800.sql
@@ -19,6 +19,6 @@ CREATE TABLE %TABLE_PREFIX%record_search_key_oa_compliance (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 insert into %TABLE_PREFIX%search_key (sek_id, sek_namespace, sek_incr_id, sek_title, sek_alt_title, sek_desc, sek_adv_visible, sek_simple_used, sek_myfez_visible, sek_order, sek_html_input, sek_fez_variable, sek_smarty_variable, sek_cvo_id, sek_lookup_function, sek_data_type, sek_relationship, sek_meta_header, sek_cardinality, sek_suggest_function, sek_comment_function, sek_faceting, sek_derived_function, sek_lookup_id_function, sek_bulkchange)
-values('UQ_54','UQ','54','OA Compliance','','','0','0','0','999','contvocab','none','','453570','','varchar','1','','0','','','0','','','0');
+values('core_113','core','54','OA Compliance','','','0','0','0','999','contvocab','none','','453570','','varchar','1','','0','','','0','','','0');
 insert into %TABLE_PREFIX%search_key (sek_id, sek_namespace, sek_incr_id, sek_title, sek_alt_title, sek_desc, sek_adv_visible, sek_simple_used, sek_myfez_visible, sek_order, sek_html_input, sek_fez_variable, sek_smarty_variable, sek_cvo_id, sek_lookup_function, sek_data_type, sek_relationship, sek_meta_header, sek_cardinality, sek_suggest_function, sek_comment_function, sek_faceting, sek_derived_function, sek_lookup_id_function, sek_bulkchange)
-values('UQ_55','UQ','55','OA Notes','','','0','0','0','999','text','none','','453235','','varchar','1','','0','','','0','','','0');
+values('core_114','core','55','OA Notes','','','0','0','0','999','text','none','','453235','','varchar','1','','0','','','0','','','0');
