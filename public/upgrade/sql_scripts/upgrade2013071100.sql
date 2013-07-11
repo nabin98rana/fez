a094bcc5950cdcc0d3827fc7a1606cc0e099b4e2
Andrew Martlew <a.martlew@library.uq.edu.au>
2013-07-11 15:14:19 +1000

    - *REQUIRES SQL UPGRADE* - Scopus queue/sessions tables - Relates to story https://www.pivotaltracker.com/story/show/48130319

---
+++ public/upgrade/sql_scripts/upgrade2013071100.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS %TABLE_PREFIX%scopus_queue (
+  spq_key int(10) unsigned NOT NULL AUTO_INCREMENT,
+  spq_id varchar(128) NOT NULL DEFAULT '',
+  spq_op varchar(5) NOT NULL DEFAULT '',
+  PRIMARY KEY (spq_key),
+  UNIQUE KEY id_op (spq_id,spq_op)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+CREATE TABLE IF NOT EXISTS %TABLE_PREFIX%scopus_session (
+  scs_id int(11) NOT NULL AUTO_INCREMENT,
+  scs_tok varchar(400) NOT NULL,
+  scs_ts timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (scs_id)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
