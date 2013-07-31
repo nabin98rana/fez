0e1037871eb9776d9b4affa41cb9574b94789822
Christiaan Kortekaas <ck@uq.edu.au>
2013-07-31 13:51:41 +1000

    Added a hover/helper for the open access release date label - relates to story https://www.pivotaltracker.com/story/show/53448631 #bing hovercat

--- public/js/swfuploader.js
+++ public/js/swfuploader.js
@@ -373,7 +373,7 @@ function swfuploaderAddDatatableEntry(entry) {
 
     if (showFileUploadExtras == 1) {
         var sizeCell2 = row2.insertCell(2);
-        sizeCell2.innerHTML ='Open Access Release Date <br /><input type="text" id="datepicker'+entry.id+'" name="embargo_date['+entry.id.slice(-1)+']" />'
+        sizeCell2.innerHTML ='<a class="form_note"><span class="form_note">date that the file can be made publicly available</span>Open Access Release Date</a> <br /><input type="text" id="datepicker'+entry.id+'" name="embargo_date['+entry.id.slice(-1)+']" />'
         $( "#datepicker"+entry.id ).datepicker({dateFormat : 'dd-mm-yy'});
         var sizeCell2 = row2.insertCell(3);
     }
