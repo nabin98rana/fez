9224cb61687589582e3ea1c252daca7fb6756d59
Christiaan Kortekaas <ck@uq.edu.au>
2013-11-13 16:43:32 +1000

    *requires sql upgrade/review..* Fixed scopus import primary key [#57031192]

--- public/upgrade/sql_scripts/upgrade2013111300.sql
+++ public/upgrade/sql_scripts/upgrade2013111300.sql
@@ -1,10 +1,10 @@
 CREATE TABLE %TABLE_PREFIX%scopus_import_stats (
   scs_pid varchar(64) DEFAULT NULL,
-  scs_contrib_id TEXT DEFAULT NULL,
+  scs_contrib_id varchar(255) DEFAULT NULL,
   scs_operation varchar(8) DEFAULT NULL,
   scs_count int(11) DEFAULT NULL,
   scs_doc_type varchar(100) DEFAULT NULL,
   scs_ag_type varchar(100) DEFAULT NULL,
   scs_title TEXT DEFAULT NULL,
-  PRIMARY KEY (scs_pid)
+  PRIMARY KEY (scs_contrib_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8
