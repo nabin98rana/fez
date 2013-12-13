e5fec5dc5c35518c7cec6dc6b6c1b534ea7e2846
Aaron Brown <a.brown@library.uq.edu.au>
2013-12-13 11:31:54 +1000

    *** sql upgrade ***  This sets the link cheching table to allow longer urls. Since the url is a primary key it limits the index to the maximum of 255 characters.
    [#52606919]

---
+++ public/upgrade/sql_scripts/upgrade2013121300.sql
@@ -0,0 +1,1 @@
+ALTER TABLE %TABLE_PREFIX%link_status_reports CHANGE lsr_url lsr_url VARCHAR(1024) CHARSET utf8 COLLATE utf8_general_ci NOT NULL, DROP PRIMARY KEY, ADD PRIMARY KEY (lsr_url(255)), DROP INDEX idx_lsr_url, ADD UNIQUE INDEX idx_lsr_url (lsr_url(255));
