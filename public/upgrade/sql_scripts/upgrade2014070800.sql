a39f4fd6f352e5878ec06f9f3b0c3a0c4802e7bb
Aaron Brown <a.brown@library.uq.edu.au>
2014-07-09 16:18:00 +1000

    Requires SQL upgrade Config form change for ERA blacklist
    [#74576424]

---
+++ public/upgrade/sql_scripts/upgrade2014070800.sql
@@ -0,0 +1,4 @@
+insert into %TABLE_PREFIX%config (config_name, config_module, config_value) values('app_era_blacklist_collection','core',NULL);
+insert into %TABLE_PREFIX%config (config_name, config_module, config_value) values('app_nhmrc_guideline_collection','core',NULL);
+
+
