8b5882a7c23361153063be88b5bc986505753a4c
Aaron Brown <a.brown@library.uq.edu.au>
2014-10-30 12:22:26 +1000

    Fix OAI issue

--- public/oai.php
+++ public/oai.php
@@ -463,7 +463,7 @@ if (!empty($verb)) {
                                     $record_dataset_of = new RecordObject($dataset_of_pid);
                                     $list[$i]['rek_isdatasetoftitle'][$j] = $record_dataset_of->getTitle();
                                     $list[$i]['rek_isdatasetof_doi'][$j] = Record::getSearchKeyIndexValue($dataset_of_pid, "doi");
-                                    $list[$i]['rek_isdatasetof_publication'][$j] = ($data_collection_xdis_id != Record::getSearchKeyIndexValue($dataset_of_pid, "Display Type"), false);
+                                    $list[$i]['rek_isdatasetof_publication'][$j] = ($data_collection_xdis_id != Record::getSearchKeyIndexValue($dataset_of_pid, "Display Type", false));
                                 }
                             }
 
