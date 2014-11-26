4c089f1bf395bad33194c804684e97fa3d234bd1
Aaron Brown <a.brown@library.uq.edu.au>
2014-11-26 10:43:00 +1000

    Add info in logs to help debugging.

--- public/include/class.log.php
+++ public/include/class.log.php
@@ -70,6 +70,27 @@ class FezLog
 		Zend_Registry::set('fezlog', $this);
 	}
 
+
+    //Pretty stacktrace
+    private function generateStacktrace()
+    {
+        $e = new Exception();
+        $trace = explode("\n", $e->getTraceAsString());
+        // reverse array to make steps line up chronologically
+        $trace = array_reverse($trace);
+        array_shift($trace); // remove {main}
+        array_pop($trace); // remove call to this method
+        $length = count($trace);
+        $result = array();
+
+        for ($i = 0; $i < $length; $i++)
+        {
+            $result[] = ($i + 1)  . ')' . substr($trace[$i], strpos($trace[$i], ' ')); // replace '#someNum' with '$i)', set the right ordering
+        }
+
+        return "\t" . implode("\n\t", $result);
+    }
+
 	/**
 	 *
 	 * @return FezLog
@@ -106,6 +127,7 @@ class FezLog
 
 	public function err($message)
 	{
+        $message.= "\n" . $this->generateStacktrace() . "\n";
 		foreach($this->_logs as $log)
 			$log['log']->err($this->format_message($log['type'], $message));
 	}
