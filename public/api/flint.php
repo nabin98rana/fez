fef4e43752844782ba974d0f602d32793bc9c38e
Aaron Brown <a.brown@library.uq.edu.au>
2015-04-27 11:51:36 +1000

    Flint  API https://www.pivotaltracker.com/story/show/92490110

---
+++ public/api/flint.php
@@ -0,0 +1,52 @@
+<?php
+/* vim: set expandtab tabstop=4 shiftwidth=4: */
+// +----------------------------------------------------------------------+
+// | Fez - Digital Repository System                                      |
+// +----------------------------------------------------------------------+
+// | Copyright (c) 2005, 2006, 2007 The University of Queensland,         |
+// | Australian Partnership for Sustainable Repositories,                 |
+// | eScholarship Project                                                 |
+// |                                                                      |
+// | Some of the Fez code was derived from Eventum (Copyright 2003, 2004  |
+// | MySQL AB - http://dev.mysql.com/downloads/other/eventum/ - GPL)      |
+// |                                                                      |
+// | This program is free software; you can redistribute it and/or modify |
+// | it under the terms of the GNU General Public License as published by |
+// | the Free Software Foundation; either version 2 of the License, or    |
+// | (at your option) any later version.                                  |
+// |                                                                      |
+// | This program is distributed in the hope that it will be useful,      |
+// | but WITHOUT ANY WARRANTY; without even the implied warranty of       |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        |
+// | GNU General Public License for more details.                         |
+// |                                                                      |
+// | You should have received a copy of the GNU General Public License    |
+// | along with this program; if not, write to:                           |
+// |                                                                      |
+// | Free Software Foundation, Inc.                                       |
+// | 59 Temple Place - Suite 330                                          |
+// | Boston, MA 02111-1307, USA.                                          |
+// +----------------------------------------------------------------------+
+// | Authors: Aaron Brown <a.brown@library.uq.edu.au>                     |
+// +----------------------------------------------------------------------+
+//
+//  authi_arg_id = '11' is assumed to give all lister permissions for public access. It is not necessarily aways the the case it's this simple.
+//
+
+
+include_once('../config.inc.php');
+include_once(APP_INC_PATH . "class.db_api.php");
+include_once(APP_INC_PATH . "class.flint.php");
+
+$callback = $_GET['callback'];
+$callback = !empty($callback) ? preg_replace('/[^a-z0-9\.$_]/si', '', $callback) : false;
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: ' . ($callback ? 'application/javascript' : 'application/json') . ';charset=UTF-8');
+
+if(!empty($_REQUEST['languages'])) {
+    echo json_encode(Flint::returnFlintLanguages());
+} else if(!empty($_REQUEST['interviewee'])) {
+    echo json_encode(Flint::returnInterviewees());
+} else {
+    echo json_encode(array(), JSON_FORCE_OBJECT);
+}
---
+++ public/include/class.flint.php
@@ -0,0 +1,49 @@
+<?php
+
+include_once(dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR."config.inc.php");
+require_once(__DIR__ . '/class.record.php');
+require_once(__DIR__ . '/class.datastream.php');
+
+/**
+ * Class that contains utilities for Flint.
+ *
+ */
+class Flint
+{
+    function returnFlintLanguages() {
+        $log = FezLog::get();
+        $db = DB_API::get();
+        $stmt = "SELECT B.*, COUNT(rek_subject) AS record_count FROM " . APP_TABLE_PREFIX . "controlled_vocab_relationship
+            LEFT JOIN " . APP_TABLE_PREFIX . "controlled_vocab AS A  ON cvo_id = cvr_parent_cvo_id
+            LEFT JOIN " . APP_TABLE_PREFIX . "controlled_vocab AS B ON B.cvo_id = cvr_child_cvo_id
+            LEFT JOIN " . APP_TABLE_PREFIX . "record_search_key_subject ON rek_subject = cvr_child_cvo_id
+            LEFT JOIN " . APP_TABLE_PREFIX . "record_search_key ON rek_subject_pid = rek_pid AND rek_status = '2'
+            WHERE A.cvo_title = 'AIATSIS codes' GROUP BY B.cvo_id";
+
+        try {
+            $res = $db->fetchAll($stmt);
+        }
+        catch (Exception $ex) {
+            $log->err($ex);
+            return false;
+        }
+        return $res;
+    }
+
+    function returnInterviewees() {
+        $log = FezLog::get();
+        $db = DB_API::get();
+        $stmt = "    SELECT DISTINCT(rek_contributor) FROM " . APP_TABLE_PREFIX . "record_search_key_contributor
+        LEFT JOIN " . APP_TABLE_PREFIX . "record_search_key_ismemberof ON rek_ismemberof_pid = rek_contributor_pid
+        WHERE rek_ismemberof IN ('UQ:353627');";
+
+        try {
+            $res = $db->fetchAll($stmt);
+        }
+        catch (Exception $ex) {
+            $log->err($ex);
+            return false;
+        }
+        return $res;
+    }
+}
