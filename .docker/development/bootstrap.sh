797ddeadb7dd917c3597a254a9c8daf8fb8e4f07
Christiaan Kortekaas <ck@uq.edu.au>
2015-05-05 15:53:24 +1000

    replaced ip with YOUR_IP

--- .docker/development/bootstrap.sh
+++ .docker/development/bootstrap.sh
@@ -11,4 +11,4 @@ echo '127.0.0.1   dev-fez.library.uq.edu.au' >> /etc/hosts
 # Comment out the below if you want to disable xdebug
 #rm -f /etc/php.d/15-xdebug.ini
 # Or uncomment this to use xdebug in cli processes
-#echo 'xdebug.remote_host=10.26.99.102' >> /etc/php.d/15-xdebug.ini
+#echo 'xdebug.remote_host=YOURIP' >> /etc/php.d/15-xdebug.ini
