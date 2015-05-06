3a87e44637ea022e8264f5d597887b165c6c6697
Christiaan Kortekaas <ck@uq.edu.au>
2015-05-06 14:15:38 +1000

    Clearer instructions for xdebug in bootstrap.sh

--- .docker/development/bootstrap.sh
+++ .docker/development/bootstrap.sh
@@ -11,4 +11,5 @@ echo '127.0.0.1   dev-fez.library.uq.edu.au' >> /etc/hosts
 # Comment out the below if you want to disable xdebug
 #rm -f /etc/php.d/15-xdebug.ini
 # Or uncomment this to use xdebug in cli processes
-#echo 'xdebug.remote_host=YOURIP' >> /etc/php.d/15-xdebug.ini
+# Set the ip to what is in your docker0 interface when you run ipconfig
+#echo 'xdebug.remote_host=YOUR_docker0_IP' >> /etc/php.d/15-xdebug.ini
