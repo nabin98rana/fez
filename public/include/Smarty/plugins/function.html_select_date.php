29f5189a8a1d73c34d69b55692eac26ad449d51a
Aaron Brown <a.brown@library.uq.edu.au>
2015-05-14 16:28:38 +1000

    Fix for null dates in the template.
    https://github.com/uqlibrary/fez/issues/24

--- public/include/Smarty/plugins/function.html_select_date.php
+++ public/include/Smarty/plugins/function.html_select_date.php
@@ -64,7 +64,9 @@ function smarty_function_html_select_date($params)
     }
 
     //This is need for backward compatibility to Smart 2.x
-    $params['time'] = trim($params['time']);
+    if ($params['time'] !== null) {
+        $params['time'] = trim($params['time']);
+    }
     if(strlen($params['time']) == 10) {
         if(substr($params['time'], -2, 2) == '00') {
             $params['day_empty'] = "";
