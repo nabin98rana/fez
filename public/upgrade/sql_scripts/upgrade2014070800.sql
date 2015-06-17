75ac25c5d3b1105b54bfa3b48aab6437fc0c5b51
uqabro13 <uqabro13@lib-ab-fed.library.uq.edu.au>
2015-06-17 10:03:44 +1000

    Change the upgrade scripts for the docker install so it doesn't conflict with any current values.

--- public/upgrade/sql_scripts/upgrade2014070800.sql
+++ public/upgrade/sql_scripts/upgrade2014070800.sql
@@ -1,4 +1,4 @@
-insert into %TABLE_PREFIX%config (config_name, config_module, config_value) values('app_era_blacklist_collection','core',NULL);
-insert into %TABLE_PREFIX%config (config_name, config_module, config_value) values('app_nhmrc_guideline_collection','core',NULL);
+insert IGNORE into %TABLE_PREFIX%config (config_name, config_module, config_value) values('app_era_blacklist_collection','core',NULL);
+insert IGNORE into %TABLE_PREFIX%config (config_name, config_module, config_value) values('app_nhmrc_guideline_collection','core',NULL);
 
 
