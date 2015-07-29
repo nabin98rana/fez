d32d04892c0219d4f3f4c302541b4d361fe6c59b
uqabro13 <uqabro13@lib-ab-fed.library.uq.edu.au>
2015-07-29 11:14:30 +1000

    Fix up annoying icon that's almost never used.
    https://www.pivotaltracker.com/story/show/98791504

--- public/upgrade/sql_scripts/upgrade2013102200.sql
+++ public/upgrade/sql_scripts/upgrade2013102200.sql
@@ -1,4 +1,4 @@
-INSERT INTO IGNORE %TABLE_PREFIX%config (config_name, config_module) VALUES ('crossref_doi_prefix', 'core');
-INSERT INTO IGNORE %TABLE_PREFIX%config (config_name, config_module) VALUES ('crossref_service_password', 'core');
-INSERT INTO IGNORE %TABLE_PREFIX%config (config_name, config_module) VALUES ('crossref_service_username', 'core');
-INSERT INTO IGNORE %TABLE_PREFIX%config (config_name, config_module) VALUES ('crossref_upload_service', 'core');
+INSERT IGNORE INTO %TABLE_PREFIX%config (config_name, config_module) VALUES ('crossref_doi_prefix', 'core');
+INSERT IGNORE INTO %TABLE_PREFIX%config (config_name, config_module) VALUES ('crossref_service_password', 'core');
+INSERT IGNORE INTO %TABLE_PREFIX%config (config_name, config_module) VALUES ('crossref_service_username', 'core');
+INSERT IGNORE INTO %TABLE_PREFIX%config (config_name, config_module) VALUES ('crossref_upload_service', 'core');
