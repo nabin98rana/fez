e5384edcbcc28e1d1f671729920767e2825aa437
Andrew Martlew <andrew@martlew.id.au>
2015-11-12 09:24:14 +1000

    Adding solr local file configs

---
+++ public/upgrade/sql_scripts/upgrade2015111200.sql
@@ -0,0 +1,2 @@
+insert IGNORE into %TABLE_PREFIX%config (config_name, config_module, config_value) values('app_solr_local_file','core','OFF');
+insert IGNORE into %TABLE_PREFIX%config (config_name, config_module, config_value) values('app_solr_local_file_path','core',NULL);
