45384fcd86577227bbdc500b3aa6a0c71a099e59
Andrew Martlew <andrew@martlew.id.au>
2015-11-24 11:39:39 +1000

    Adding the login helper file

---
+++ tests/application/features/bootstrap/LoginHelper.php
@@ -0,0 +1,60 @@
+<?php
+
+class loginHelper {
+  /**
+   * @Given /^I login as administrator$/
+   */
+  static function iLoginAsAdministrator($page) {
+    $page->visit("/login.php");
+    $page->fillField("username", "admin_test");
+    $page->fillField("passwd", "Ilovedonkey5");
+    $page->pressButton("Login");
+    $page->visit("/");
+
+  }
+
+  /**
+   * @Given /^I login as UPO$/
+   */
+  static function iLoginAsUPO($page) {
+    $page->visit("/login.php");
+    $page->fillField("username", "upo_test");
+    $page->fillField("passwd", "Ilovedonkey5");
+    $page->pressButton("Login");
+    $page->visit("/");
+
+  }
+
+  /**
+   * @Given /^I login as super administrator$/
+   */
+  static function iLoginAsSuperAdministrator($page) {
+    $page->visit("/login.php");
+    $page->fillField("username", "superadmin_test");
+    $page->fillField("passwd", "Ilovedonkey5");
+    $page->pressButton("Login");
+    $page->visit("/");
+  }
+
+  /**
+   * @Given /^I login as user no groups$/
+   */
+  static function iLoginAsUserNoGroups($page) {
+    $page->visit("/login.php");
+    $page->fillField("username", "user_test");
+    $page->fillField("passwd", "Ilovedonkey5");
+    $page->pressButton("Login");
+    $page->visit("/");
+  }
+
+  /**
+   * @Given /^I login as thesis officer$/
+   */
+  static function iLoginAsThesisOfficer($page) {
+    $page->visit("/login.php");
+    $page->fillField("username", "thesisofficer_test");
+    $page->fillField("passwd", "Ilovedonkey5");
+    $page->pressButton("Login");
+    $page->visit("/");
+  }
+}
