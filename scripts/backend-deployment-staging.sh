7fb44a7a8a0cd862ca942f9a11163026f41bf2d9
ckortekaas <ck@uq.edu.au>
2015-12-08 12:17:56 +1000

    Fixed service name for fezstaging deployment (fez -> fezstaging)

--- scripts/backend-deployment-staging.sh
+++ scripts/backend-deployment-staging.sh
@@ -10,7 +10,7 @@ echo Deploying ${CI_COMMIT_ID}..
 SQS_MESSAGE=$(<${BASE_DIR}/.docker/staging/aws-task-definition.json)
 SQS_MESSAGE="${SQS_MESSAGE//\<COMMIT_HASH\>/${CI_COMMIT_ID}}"
 SQS_MESSAGE="${SQS_MESSAGE//\<NEWRELIC_LICENSE\>/${NEWRELIC_LICENSE}}"
-SQS_MESSAGE_ATTRIBUTES='{"service": { "StringValue": "fez", "DataType": "String" } }'
+SQS_MESSAGE_ATTRIBUTES='{"service": { "StringValue": "fezstaging", "DataType": "String" } }'
 
 aws sqs send-message \
   --queue-url ${SQS_URL} \
