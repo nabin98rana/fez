ed442fe3a24e1ced2e3a09c7598f5bbbe999fe48
ckortekaas <ck@uq.edu.au>
2016-03-18 15:30:22 +1000

    Added S3 functions and related configuration files *requires sql upgrade*

---
+++ public/upgrade/sql_scripts/upgrade2016031800.sql
@@ -0,0 +1,4 @@
+insert IGNORE into %TABLE_PREFIX%config (config_name, config_module, config_value) values('aws_file_serve_url','core','');
+insert IGNORE into %TABLE_PREFIX%config (config_name, config_module, config_value) values('aws_s3_bucket','core','');
+insert IGNORE into %TABLE_PREFIX%config (config_name, config_module, config_value) values('aws_cf_private_key_file','core','');
+insert IGNORE into %TABLE_PREFIX%config (config_name, config_module, config_value) values('aws_cf_key_pair_id','core','');
