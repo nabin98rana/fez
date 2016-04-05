302ca241c8b959f9e583dce062a75571f0250db7
ckortekaas <ck@uq.edu.au>
2016-04-05 16:36:50 +1000

    Added ResearcherID and 'network file upload' mount creds/fstabs to staging bootstrap

--- .docker/development/backend/fpm/bootstrap.sh
+++ .docker/development/backend/fpm/bootstrap.sh
@@ -11,6 +11,12 @@ cd ${BASE_DIR}/.docker/development
 #sed -i "s|xdebug.remote_enable=1|xdebug.remote_enable=0\nxdebug.remote_host=<your_ip_here>"
 
 aws s3 cp s3://uql/fez/fez_staging_cloudfront_private_key.pem ${BASE_DIR}/data/
+# for the mounts
+aws s3 cp s3://uql/fez/espace.credentials ${BASE_DIR}/data/
+aws s3 cp s3://uql/fez/libtools.credentials ${BASE_DIR}/data/
+aws s3 cp s3://uql/fez/espace.fstab ${BASE_DIR}/data/
+cat ${BASE_DIR}/data/espace.fstab >> /etc/fstab
+
 
 if [ "${APP_ENVIRONMENT}" == "testing" ]; then
   rm -f /etc/php.d/15-xdebug.ini
