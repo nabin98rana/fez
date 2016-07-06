77fccdbb4b2e4c65880f4b69d877ad09510d315e
Cameron Green <cam@uq.edu.au>
2016-07-06 11:57:28 +1000

    Adding admin screen

---
+++ public/upgrade/sql_scripts/upgrade2016070600.sql
@@ -0,0 +1,5 @@
+insert IGNORE into %TABLE_PREFIX%config (config_name, config_module, config_value) values('sinet_load_enabled','core','');
+insert IGNORE into %TABLE_PREFIX%config (config_name, config_module, config_value) values('sinet_load_db_host','core','');
+insert IGNORE into %TABLE_PREFIX%config (config_name, config_module, config_value) values('sinet_load_db_name','core','');
+insert IGNORE into %TABLE_PREFIX%config (config_name, config_module, config_value) values('sinet_load_db_user','core','');
+insert IGNORE into %TABLE_PREFIX%config (config_name, config_module, config_value) values('sinet_load_db_passwd','core','');
