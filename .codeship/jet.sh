c7eaf9ec986f3ed08fff0822b3c1dbecc556060e
ckortekaas <ck@uq.edu.au>
2016-07-12 07:54:55 +1000

    Jet tests running 59/71 scenarios passing [#126013699]

---
+++ .codeship/jet.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+
+
+echo Starting test run..
+
+# TODO: replace with MySQL check - yum install mysql is slow enough it seems
+#sleep 30
+
+
+
+
+
+yum -y install mysql
+
+i=0
+MYSQL_HEALTH_CMD="mysqladmin ping -hfezdb -ufez -pfez"
+HEALTH_MSG=$(${MYSQL_HEALTH_CMD} 2>&1)
+while ! [[ -n "${HEALTH_MSG}" && ${HEALTH_MSG} != *"failed"* && ${HEALTH_MSG} != *"denied"* ]]; do
+  i=`expr ${i} + 1`
+  if [ ${i} -ge ${MAX_LOOPS} ]; then
+    echo "$(date) - MySQL still not reachable, giving up"
+    exit 1
+  fi
+  echo "$(date) - waiting for MySQL..."
+  sleep 1
+  HEALTH_MSG=$(${MYSQL_HEALTH_CMD} 2>&1)
+done
+
+cd /var/app/current/tests/application && php init.php
+
+echo Starting upgrading..
+UPGRADE_RES=$(curl -s http://fez/upgrade/index.php?upgradeOnly=1 | grep succeeded)
+if [ "${UPGRADE_RES}" == "" ]; then
+  echo "failed to run upgrade scripts! :("
+  exit 1
+fi
+
+echo Seeding SQL data..
+
+CONTAINER_DB_SEED_DIR=/var/app/current/.docker/development/backend/db/seed
+mysql -uroot -pdevelopment -hfezdb mysql < ${CONTAINER_DB_SEED_DIR}/installdb.sql
+mysql -uroot -pdevelopment -hfezdb fez < ${CONTAINER_DB_SEED_DIR}/citation.sql
+mysql -uroot -pdevelopment -hfezdb fez < ${CONTAINER_DB_SEED_DIR}/cvs.sql
+mysql -uroot -pdevelopment -hfezdb fez < ${CONTAINER_DB_SEED_DIR}/development.sql
+mysql -uroot -pdevelopment -hfezdb fez < ${CONTAINER_DB_SEED_DIR}/workflows.sql
+mysql -uroot -pdevelopment -hfezdb fez < ${CONTAINER_DB_SEED_DIR}/xsd.sql
+mysql -uroot -pdevelopment -hfezdb fez < ${CONTAINER_DB_SEED_DIR}/jetsetup.sql
+
+echo Running tests..
+
+cd /var/app/current/
+./tests/application/run-tests.sh
+#docker -v
+#docker-compose -v
