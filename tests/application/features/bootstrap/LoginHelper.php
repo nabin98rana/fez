81a1a4ea7c1b86915a6b6b7a55a466d0822f862a
ckortekaas <ck@uq.edu.au>
2016-07-15 10:00:00 +1000

    :hourglass: improved wait checks for login helper [#126013699]

--- tests/application/features/bootstrap/LoginHelper.php
+++ tests/application/features/bootstrap/LoginHelper.php
@@ -1,12 +1,22 @@
 <?php
 
 class loginHelper {
+
+  /* Wait for the logout/login pages to render correctly
+   *
+   *
+   */
+  private function waitForLogoutLogin($page) {
+    $page->getSession()->wait(5000, "dojo.byId('powered-by')");
+    $page->visit("/login.php");
+    $page->getSession()->wait(10000, "dojo.byId('powered-by')");
+  }
+
   /**
    * @Given /^I login as administrator$/
    */
-  static function iLoginAsAdministrator($page) {
-    $page->visit("/login.php");
-    $page->getSession()->wait(5000, "dojo.byId('powered-by')");
+  public function iLoginAsAdministrator($page) {
+    $this->waitForLogoutLogin($page);
     $page->fillField("username", "admin_test");
     $page->fillField("passwd", "Ilovedonkey5");
     $page->pressButton("Login");
@@ -17,9 +27,8 @@ class loginHelper {
   /**
    * @Given /^I login as UPO$/
    */
-  static function iLoginAsUPO($page) {
-    $page->visit("/login.php");
-    $page->getSession()->wait(5000, "dojo.byId('powered-by')");
+  public function iLoginAsUPO($page) {
+    $this->waitForLogoutLogin($page);
     $page->fillField("username", "upo_test");
     $page->fillField("passwd", "Ilovedonkey5");
     $page->pressButton("Login");
@@ -30,9 +39,8 @@ class loginHelper {
   /**
    * @Given /^I login as super administrator$/
    */
-  static function iLoginAsSuperAdministrator($page) {
-    $page->visit("/login.php");
-    $page->getSession()->wait(5000, "dojo.byId('powered-by')");
+  public function iLoginAsSuperAdministrator($page) {
+    $this->waitForLogoutLogin($page);
     $page->fillField("username", "superadmin_test");
     $page->fillField("passwd", "Ilovedonkey5");
     $page->pressButton("Login");
@@ -42,9 +50,8 @@ class loginHelper {
   /**
    * @Given /^I login as user no groups$/
    */
-  static function iLoginAsUserNoGroups($page) {
-    $page->visit("/login.php");
-    $page->getSession()->wait(5000, "dojo.byId('powered-by')");
+  public function iLoginAsUserNoGroups($page) {
+    $this->waitForLogoutLogin($page);
     $page->fillField("username", "user_test");
     $page->fillField("passwd", "Ilovedonkey5");
     $page->pressButton("Login");
@@ -54,9 +61,8 @@ class loginHelper {
   /**
    * @Given /^I login as thesis officer$/
    */
-  static function iLoginAsThesisOfficer($page) {
-    $page->visit("/login.php");
-    $page->getSession()->wait(5000, "dojo.byId('powered-by')");
+  public function iLoginAsThesisOfficer($page) {
+    $this->waitForLogoutLogin($page);
     $page->fillField("username", "thesisofficer_test");
     $page->fillField("passwd", "Ilovedonkey5");
     $page->pressButton("Login");
