c647a5f75b29b72f75aded54b67bd231b60f57ad
ckortekaas <ck@uq.edu.au>
2016-07-18 22:38:31 +1000

    :mag: Trying test stability with solr disabled [#126013699]

--- .docker/development/backend/db/seed/jetsetup.sql
+++ .docker/development/backend/db/seed/jetsetup.sql
@@ -1,1 +1,3 @@
 UPDATE fez_config SET config_value = 'fez' WHERE config_name = 'app_hostname';
+UPDATE fez_config SET config_value = 'false' WHERE config_name = 'app_solr_switch';
+UPDATE fez_config SET config_value = 'false' WHERE config_name = 'app_solr_indexer';
