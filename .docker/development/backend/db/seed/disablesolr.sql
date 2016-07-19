1b4a9c6c870f65aff5771ee18e28666e38cd900f
ckortekaas <ck@uq.edu.au>
2016-07-19 11:25:10 +1000

    :mag: only disable solr in jet testing, leave enabled in docker-compose-testing  [#126013699]

---
+++ .docker/development/backend/db/seed/disablesolr.sql
@@ -0,0 +1,2 @@
+UPDATE fez_config SET config_value = 'false' WHERE config_name = 'app_solr_switch';
+UPDATE fez_config SET config_value = 'false' WHERE config_name = 'app_solr_indexer';
--- .docker/development/backend/db/seed/jetsetup.sql
+++ .docker/development/backend/db/seed/jetsetup.sql
@@ -1,3 +1,1 @@
 UPDATE fez_config SET config_value = 'fez' WHERE config_name = 'app_hostname';
-UPDATE fez_config SET config_value = 'false' WHERE config_name = 'app_solr_switch';
-UPDATE fez_config SET config_value = 'false' WHERE config_name = 'app_solr_indexer';
