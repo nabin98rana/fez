90963e6e2867102855d01e010328b612723aaf44
Andrew Martlew <andrew@martlew.id.au>
2016-09-13 09:27:27 +1000

    Additional config value for S3 cache bucket

--- .docker/development/backend/db/seed/aws.sql
+++ .docker/development/backend/db/seed/aws.sql
@@ -3,6 +3,7 @@ UPDATE fez_config SET config_value = 'true' WHERE config_name = 'aws_s3_enabled'
 UPDATE fez_config SET config_value = '%AWS_ACCESS_KEY_ID%' WHERE config_name = 'aws_key';
 UPDATE fez_config SET config_value = '%AWS_SECRET_ACCESS_KEY%' WHERE config_name = 'aws_secret';
 UPDATE fez_config SET config_value = '%FEZ_S3_BUCKET%' WHERE config_name = 'aws_s3_bucket';
+UPDATE fez_config SET config_value = '%FEZ_S3_CACHE_BUCKET%' WHERE config_name = 'aws_s3_cache_bucket';
 UPDATE fez_config SET config_value = '%FEZ_S3_SRC_PREFIX%' WHERE config_name = 'aws_s3_src_prefix';
 UPDATE fez_config SET config_value = '%FEZ_S3_BUCKET%' WHERE config_name = 'aws_s3_san_import_bucket';
 UPDATE fez_config SET config_value = '%AWS_CLOUDFRONT_FILE_SERVE_URL%' WHERE config_name = 'aws_file_serve_url';
