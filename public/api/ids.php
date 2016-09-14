ed533633db94b1ec74236cc667d69e4406cbf5aa
Cameron Green <cam@uq.edu.au>
2016-09-14 20:55:40 +1000

    Dont allow lists to be cached

--- public/api/ids.php
+++ public/api/ids.php
@@ -71,6 +71,11 @@ if ($securityToken != APP_API_IDS_TOKEN) {
 if (!empty($list)) {
     $result = ApiResearchers::listId($author_username);
 
+    // don't allow this to be cached, should be real time
+    header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Pragma: no-cache");
+
     if ($result) {
       if (!empty($id_type)){
           foreach($result as $details) {
