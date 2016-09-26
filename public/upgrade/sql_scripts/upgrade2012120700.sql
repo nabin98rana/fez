e9b034156fed0fbaf0b6286bb543e89a79d7d170
ckortekaas <ck@uq.edu.au>
2016-09-26 15:48:55 +1000

    :hourglass_flowing_sand: Fixed data type of copyright int->varchar (as checkboxes store as on/off) strings [#130793921]

--- public/upgrade/sql_scripts/upgrade2012120700.sql
+++ public/upgrade/sql_scripts/upgrade2012120700.sql
@@ -37,5 +37,5 @@ INSERT IGNORE INTO %TABLE_PREFIX%search_key (sek_id,sek_namespace,sek_incr_id,se
 	VALUES ('core_110','core','110','SEO Code','','0','0','0','0','0','contvocab','none','',450779,'Controlled_Vocab::getTitle','int','1','','1','','0','','0','');
 
 INSERT IGNORE INTO %TABLE_PREFIX%search_key (sek_id,sek_namespace,sek_incr_id,sek_title,sek_alt_title,sek_desc,sek_adv_visible,sek_simple_used,sek_myfez_visible,sek_order,sek_html_input,sek_fez_variable,sek_smarty_variable,sek_cvo_id,sek_lookup_function,sek_data_type,sek_relationship,sek_meta_header,sek_cardinality,sek_suggest_function,sek_faceting, sek_derived_function, sek_bulkchange, sek_lookup_id_function)
-	VALUES ('core_111','core','111','Copyright','','0','0','0','0','0','checkbox','none','',450779,'','int','0','','0','','0','','0','');
+	VALUES ('core_111','core','111','Copyright','','0','0','0','0','0','checkbox','none','',450779,'','varchar','0','','0','','0','','0','');
 
