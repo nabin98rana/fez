f04e1b299f5ab80ec814bcda492757733657aa82
Andrew Martlew <andrew@martlew.id.au>
2016-10-13 14:25:24 +1000

    Refactored basic auth to use common function in auth class

--- public/include/class.auth.php
+++ public/include/class.auth.php
@@ -2604,6 +2604,39 @@ class Auth
 
     return;
   }
+
+  public static function basicAuth($loginSuccessfulUrl)
+  {
+    if (((($_SERVER["SERVER_PORT"] != 443) && (APP_HTTPS == "ON"))) && APP_REDIRECT_CHECK != 'OFF') { //should be ssl when using basic auth
+      header('Location: ' . 'https://' . APP_HOSTNAME . rtrim(APP_RELATIVE_URL, '/') . $_SERVER['REQUEST_URI']);
+      exit;
+    }
+
+    if (!isset($_SERVER['PHP_AUTH_USER'])) {
+      header('WWW-Authenticate: Basic realm="'.APP_HOSTNAME.'"');
+      header('HTTP/1.0 401 Unauthorized');
+      echo 'You must login to access this service';
+      exit;
+    } else {
+      // Check for basic authentication (over ssl) to bypass authorisation and login the user coming directly to eserv.php (and bypass usual login)
+      if (!Auth::isValidSession($session)) { // if user not already logged in
+        //print_r($_SERVER); exit;
+        if (isset($_SERVER['PHP_AUTH_USER'])) { // check to see if there is some basic auth login..
+          $username = $_SERVER['PHP_AUTH_USER'];
+          $pw = $_SERVER['PHP_AUTH_PW'];
+          if (Auth::isCorrectPassword($username, $pw)) {
+            Auth::LoginAuthenticatedUser($username, $pw, false);
+            header ("Location: https://".APP_HOSTNAME.APP_RELATIVE_URL.$loginSuccessfulUrl);
+            exit;
+          } else {
+            header('WWW-Authenticate: Basic realm="'.APP_HOSTNAME.'"');
+            header('HTTP/1.0 401 Unauthorized');
+            exit;
+          }
+        }
+      }
+    }
+  }
 }
 
 class AuthNoFedora
