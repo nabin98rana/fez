ae69d46fbf9b8e377b70af24725cb460a9b74d0c
ckortekaas <ck@uq.edu.au>
2016-11-02 16:40:58 +1000

    :mag: disabled elasticsearch for test runs. updated selenium to 3.01 [#130801399]

--- .docker/development/backend/db/seed/disablesolr.sql
+++ .docker/development/backend/db/seed/disablesolr.sql
@@ -1,2 +1,4 @@
 UPDATE fez_config SET config_value = 'false' WHERE config_name = 'app_solr_switch';
 UPDATE fez_config SET config_value = 'false' WHERE config_name = 'app_solr_indexer';
+UPDATE fez_config SET config_value = 'false' WHERE config_name = 'app_es_switch';
+UPDATE fez_config SET config_value = 'false' WHERE config_name = 'app_es_indexer';
