1b33382ae73df3ed5a714f03e74d02c78af35d2e
Andrew Martlew <andrew@martlew.id.au>
2017-01-25 12:41:08 +1000

    Continue downloading these into the container

--- .docker/production/bootstrap.sh
+++ .docker/production/bootstrap.sh
@@ -13,6 +13,8 @@ fi
 aws s3 cp s3://uql/ecs/default/services/fezproduction/config.inc.php ${BASE_DIR}/public/config.inc.php
 aws s3 cp s3://uql/fez/fez_production_cloudfront_private_key.pem ${BASE_DIR}/data/
 aws s3 cp ${BASE_DIR}/.docker/production/fez.cron s3://uql/ecs/default/services/crond/cron.d/fezproduction
+aws s3 cp s3://uql-fez-production-cache/GeoIP.dat.gz /usr/share/GeoIP/GeoIP.dat.gz && /bin/gunzip -f /usr/share/GeoIP/GeoIP.dat.gz
+aws s3 cp s3://uql-fez-production-cache/GeoLiteCity.dat.gz /usr/share/GeoIP/GeoLiteCity.dat.gz && /bin/gunzip -f /usr/share/GeoIP/GeoLiteCity.dat.gz
 cp ${BASE_DIR}/.docker/production/robots.txt ${BASE_DIR}/public/
 chmod -R 777 ${BASE_DIR}/public/include/htmlpurifier/library/HTMLPurifier
 
