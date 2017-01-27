265de71b55591c17e83ab0db443a24cbb9705ee7
ckortekaas <ck@uq.edu.au>
2017-01-27 13:58:26 +1000

    :sparkles: Show canned search link correctly for the 'Creator Name' search key (was wrongly 'Creator') [finishes #112954527]

--- public/include/class.record_view.php
+++ public/include/class.record_view.php
@@ -160,7 +160,7 @@ class RecordView {
                                 "<a href='http://dx.doi.org/".htmlspecialchars($details[$xsd_display_fields[$dis_key]['xsdmf_id']])."'>".htmlspecialchars($details[$xsd_display_fields[$dis_key]['xsdmf_id']])."</a>";
                     }
                 }
-				if ($dis_field['sek_title'] == "Author" || $dis_field['sek_title'] == "Contributor" || $dis_field['sek_title'] == "Creator") {
+				if ($dis_field['sek_title'] == "Author" || $dis_field['sek_title'] == "Contributor" || $dis_field['sek_title'] == "Creator Name") {
 					if (!empty($details[$dis_field['xsdmf_id']])) {
 						if (is_array($details[$dis_field['xsdmf_id']])) {
 							foreach ($details[$dis_field['xsdmf_id']] as $ckey => $cdata) {
