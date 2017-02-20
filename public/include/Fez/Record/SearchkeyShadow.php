a86dbb4bf1034668393024cbb495160946e1328c
Andrew Martlew <andrew@martlew.id.au>
2017-02-20 13:40:15 +1000

    Added version column to shadow tables (and source + source checksum to core shadow table)
    ** REQUIRES SQL UPGRADE **

--- public/include/Fez/Record/SearchkeyShadow.php
+++ public/include/Fez/Record/SearchkeyShadow.php
@@ -123,7 +123,7 @@ class Fez_Record_SearchkeyShadow
     $pid = $this->_pid;
 
     $stmt = "INSERT INTO " . APP_TABLE_PREFIX . "record_search_key_" . $sekTable . "__shadow
-               SELECT *, " . $db->quote($date) . " FROM " . APP_TABLE_PREFIX . "record_search_key_" . $sekTable . "
+               SELECT *, " . $db->quote($date) . ", " . $db->quote($pid . ' ' . $date) . " FROM " . APP_TABLE_PREFIX . "record_search_key_" . $sekTable . "
                         WHERE rek_" . $sekTable . "_pid = " . $db->quote($pid);
     try {
       $res = $db->exec($stmt);
@@ -147,7 +147,7 @@ class Fez_Record_SearchkeyShadow
     $pid = $this->_pid;
 
     $stmt = "INSERT INTO " . APP_TABLE_PREFIX . "record_search_key__shadow
-               SELECT *, " . $db->quote($date) . " FROM " . APP_TABLE_PREFIX . "record_search_key
+               SELECT *, " . $db->quote($date) . ", " . $db->quote($pid . ' ' . $date) . " FROM " . APP_TABLE_PREFIX . "record_search_key
                         WHERE rek_pid = " . $db->quote($pid);
     try {
       $res = $db->exec($stmt);
@@ -205,6 +205,7 @@ class Fez_Record_SearchkeyShadow
     $log = FezLog::get();
     $db = DB_API::get();
     $pid = $this->_pid;
+    /*
     $searchKeys = Search_Key::getList(false);
     $datesArray = array();
 
@@ -226,14 +227,14 @@ class Fez_Record_SearchkeyShadow
         }
         $datesArray = array_merge($datesArray, $res);
       }
-    }
+    }*/
 
     $stmt = "SELECT
                 rek_stamp
              FROM
                 " . APP_TABLE_PREFIX . "record_search_key__shadow
              WHERE
-                rek_pid = " . $db->quote($pid) . "
+                rek_pid = " . $db->quote($pid) . " AND rek_source = 'master'
              ORDER BY rek_stamp DESC";
     try {
       $res = $db->fetchCol($stmt);
@@ -242,9 +243,9 @@ class Fez_Record_SearchkeyShadow
       return false;
     }
 
-    $datesArray = array_merge($datesArray, $res);
+    //$datesArray = array_merge($datesArray, $res);
 
-    return array_unique($datesArray);
+    return array_unique($res);
   }
 
   public function hasDelta($sek_title)
