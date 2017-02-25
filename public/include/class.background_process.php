6a03f9812d06b7bb74db6c8f660a22dfd9a2148d
ckortekaas <ck@uq.edu.au>
2017-02-25 13:12:52 +1000

    :hourglass: fixed bgp bug in last commit [#140541329]

--- public/include/class.background_process.php
+++ public/include/class.background_process.php
@@ -333,6 +333,7 @@ class BackgroundProcess {
         $this->setHostname($_SERVER['HOSTNAME']);
         $res = $this->getDetails();
         $utc_date = Date_API::getSimpleDateUTC();
+        include_once(APP_INC_PATH . $res['bgp_include']);
         $bgp = unserialize($res['bgp_serialized']);
 
         $lastHeartbeat = Date_API::dateDiff("n", $res['bgp_heartbeat'], $utc_date);
@@ -348,7 +349,7 @@ class BackgroundProcess {
             $bgp->setState(BGP_FINISHED);
             return;
         }
-        include_once(APP_INC_PATH . $res['bgp_include']);
+
 
         $msg = 'Starting BGP ID:'. $bgp->bgp_id . ' with name ' . $bgp->name . "..\n";
 
